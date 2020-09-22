d10862a75d459f4955fbd449d3a2c9f21f874498
CyrusWilkie <james.wilkiec@gmail.com>
2020-09-22 10:53:34 +1000

    Fixed up accidental files

--- src/test_channel_addowner.py
+++
@@ -1,6 +0,0 @@
-from channel import *
-from channels import *
-
-# channel_addowner should add the user with the provided u_id to the list of owners of a channel with the provided channel_id
-# assumes that u_id is already a member of the channel
-def test_channel_addowner():
