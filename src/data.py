1cee4a5bb30be3bdc57163967c529aae1c04a544
jordan <z5169771@ad.unsw.edu.au>
2020-09-25 14:14:41 +1000

    added is_admin and is_private to respective data fields

--- src/data.py
+++ src/data.py
@@ -1,9 +1,13 @@
 # can we have comments in coordination, would be easier to relate.
 
+#added admin variable to users
+#added is_private to channels
+
 data = {
     'users': [
         { #user -> u_id: 0
             'u_id' : 0,
+            'is_admin' : True,
             'email' : 'blah0@domain',
             'name_first' : 'fname0',
             'name_last' : 'lname0',
@@ -13,6 +17,7 @@ data = {
         },
         { #user -> u_id: 1
             'u_id' : 1,
+            'is_admin' : False,
             'email' : 'blah1@domain',
             'name_first' : 'fname1',
             'name_last' : 'lname1',
@@ -26,6 +31,7 @@ data = {
         { #channel0
             'channel_id' : 0,
             'name' : 'ch_name0',
+            'is_private' : True,
             'owner_members' : [ #_members
                 {
                     'u_id' : 0,
@@ -63,6 +69,7 @@ data = {
         { #channel1
             'channel_id' : 1,
             'name' : 'ch_name1',
+            'is_private' : False,
             'owner_members' : [ #_members
                 {
                     'u_id' : 1,
