a1c593699a98b643bdae0d65e8aa9af4a9a750aa
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-09-26 13:17:18 +1000

    Added intialise_user_data to test_channels_list.py

--- src/test_channels_list.py
+++ src/test_channels_list.py
@@ -1,6 +1,8 @@
-import auth
-import channel
-import channels
+from auth import auth_*
+from channel import channel_*
+from channels import channels_*
+from other import clear
+import pytest
 
 def initialise_user_data():
 
