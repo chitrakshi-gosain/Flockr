88172cdb2e34cb0950f4d62ba67aa12ac7b4f1e5
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-09-26 13:09:25 +1000

    Fixed handling of auth_register return values

--- src/test_channels_create.py
+++ src/test_channels_create.py
@@ -11,34 +11,34 @@ def initialise_user_data():
 
     # Register users:
     # Descriptive test data
-    (owner_id, owner_token) = auth_register('owner@email.com', 'owner_pass', 'owner_first', 'owner_last')
-    (user1_id, user1_token) = auth_register('user1@email.com', 'user1_pass', 'user1_first', 'user1_last')
-    (user2_id, user2_token) = auth_register('user2@email.com', 'user2_pass', 'user2_first', 'user2_last')
-    (user3_id, user3_token) = auth_register('user3@email.com', 'user3_pass', 'user3_first', 'user3_last')
-    (user4_id, user4_token) = auth_register('user4@email.com', 'user4_pass', 'user4_first', 'user4_last')
-    (user5_id, user5_token) = auth_register('user5@email.com', 'user5_pass', 'user5_first', 'user5_last')
+    owner_details = auth_register('owner@email.com', 'owner_pass', 'owner_first', 'owner_last')
+    user1_details = auth_register('user1@email.com', 'user1_pass', 'user1_first', 'user1_last')
+    user2_details = auth_register('user2@email.com', 'user2_pass', 'user2_first', 'user2_last')
+    user3_details = auth_register('user3@email.com', 'user3_pass', 'user3_first', 'user3_last')
+    user4_details = auth_register('user4@email.com', 'user4_pass', 'user4_first', 'user4_last')
+    user5_details = auth_register('user5@email.com', 'user5_pass', 'user5_first', 'user5_last')
 
     # Realistic test data
-    (john_id, john_token) = auth_register('johnsmith@gmail.com', 'qwertyuiop', 'John', 'Smith')
-    (jane_id, jane_token) = auth_register('janesmith@hotmail.com', 'mydateofbirth', 'Jane', 'Smith')
-    (noah_id, noah_token) = auth_register('noah_navarro@yahoo.com', 'aP00RPassWord', 'Noah', 'Navarro')
-    (ingrid_id, ingrid_token) = auth_register('ingrid.cline@gmail.com', '572o75630', 'Ingrid', 'Cline')
-    (donald_id, donald_token) = auth_register('donaldrichards@gmail.com', 'kjdfg;h;;df', 'Donald', 'Richards')
+    john_details = auth_register('johnsmith@gmail.com', 'qwertyuiop', 'John', 'Smith')
+    jane_details = auth_register('janesmith@hotmail.com', 'mydateofbirth', 'Jane', 'Smith')
+    noah_details = auth_register('noah_navarro@yahoo.com', 'aP00RPassWord', 'Noah', 'Navarro')
+    ingrid_details = auth_register('ingrid.cline@gmail.com', '572o75630', 'Ingrid', 'Cline')
+    donald_details = auth_register('donaldrichards@gmail.com', 'kjdfg;h;;df', 'Donald', 'Richards')
 
     # Returns user data that is implementation dependent (id, token)
     return {
-        'owner': {'id': owner_id, 'token': owner_token},
-        'user1': {'id': user1_id, 'token': user1_token},
-        'user2': {'id': user2_id, 'token': user2_token},
-        'user3': {'id': user3_id, 'token': user3_token},
-        'user4': {'id': user4_id, 'token': user4_token},
-        'user5': {'id': user5_id, 'token': user5_token},
-        'john': {'id': john_id, 'token': john_token},
-        'jane': {'id': jane_id, 'token': jane_token},
-        'noah': {'id': noah_id, 'token': noah_token},
-        'ingrid': {'id': ingrid_id, 'token': ingrid_token},
-        'donald': {'id': donald_id, 'token': donald_token}
-	}
+        'owner': owner_details,
+        'user1': user1_details,
+        'user2': user2_details,
+        'user3': user3_details,
+        'user4': user4_details,
+        'user5': user5_details,
+        'john': john_details,
+        'jane': jane_details,
+        'noah': noah_details,
+        'ingrid': ingrid_details,
+        'donald': donald_details
+    }
 
 # Creating channel with valid data
 def test_channels_create_valid_basic():
@@ -54,10 +54,10 @@ def test_channels_create_valid_basic():
     basic_channel_details = channel_details(users['owner']['token'], channel_id)
 
     assert basic_channel_details['name'] == 'A Basic Channel'
-    assert basic_channel_details['owner_members'][0]['u_id'] == users['owner']['id']
+    assert basic_channel_details['owner_members'][0]['u_id'] == users['owner']['u_id']
     assert basic_channel_details['owner_members'][0]['name_first'] == 'owner_first'
     assert basic_channel_details['owner_members'][0]['name_last'] == 'owner_last'
-    assert basic_channel_details['all_members'][0]['u_id'] == users['owner']['id']
+    assert basic_channel_details['all_members'][0]['u_id'] == users['owner']['u_id']
     assert basic_channel_details['all_members'][0]['name_first'] == 'owner_first'
     assert basic_channel_details['all_members'][0]['name_last'] == 'owner_last'
 
@@ -77,10 +77,10 @@ def test_channels_create_valid_empty():
     empty_channel_details = channel_details(users['user1']['token'], channel_id)
 
     assert empty_channel_details['name'] == ''
-    assert empty_channel_details['owner_members'][0]['u_id'] == users['user1']['id']
+    assert empty_channel_details['owner_members'][0]['u_id'] == users['user1']['u_id']
     assert empty_channel_details['owner_members'][0]['name_first'] == 'user1_first'
     assert empty_channel_details['owner_members'][0]['name_last'] == 'user1_last'
-    assert empty_channel_details['all_members'][0]['u_id'] == users['user1']['id']
+    assert empty_channel_details['all_members'][0]['u_id'] == users['user1']['u_id']
     assert empty_channel_details['all_members'][0]['name_first'] == 'user1_first'
     assert empty_channel_details['all_members'][0]['name_last'] == 'user1_last'
 
@@ -100,10 +100,10 @@ def test_channels_create_valid_private():
     private_channel_details = channel_details(users['john']['token'], channel_id)
 
     assert empty_channel_details['name'] == 'Private Disc'
-    assert empty_channel_details['owner_members'][0]['u_id'] == users['john']['id']
+    assert empty_channel_details['owner_members'][0]['u_id'] == users['john']['u_id']
     assert empty_channel_details['owner_members'][0]['name_first'] == 'John'
     assert empty_channel_details['owner_members'][0]['name_last'] == 'Smith'
-    assert empty_channel_details['all_members'][0]['u_id'] == users['john']['id']
+    assert empty_channel_details['all_members'][0]['u_id'] == users['john']['u_id']
     assert empty_channel_details['all_members'][0]['name_first'] == 'John_first'
     assert empty_channel_details['all_members'][0]['name_last'] == 'Smith_last'
 
