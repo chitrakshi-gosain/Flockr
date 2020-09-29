78867460052f76b3e7c2ce6678a105795db2de92
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-09-29 16:18:05 +1000

    Completed first implementation of channels_list and channels_listall

--- src/channels.py
+++ src/channels.py
@@ -1,20 +1,28 @@
-import data
+from data import *
+from error import AccessError, InputError
 
 def channels_list(token):
-    return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
-    }
+    '''
+    ----channels_list Documentation----
+    Parameters:
+    (token)
 
-def channels_listall(token):
+    Return Type:
+    {channels}
+
+    Exceptions:
+    N/A
+
+    Description:
+    Provide a list of all channels (and 
+    their associated details) that the 
+    authorised user is part of
+    '''
+
     # Checking token validity and finding user that accessed listall
     current_user = {}
     # Loops through users until matching token is found
-    for user in users:
+    for user in data['users']:
         if user['token'] == token:
             current_user = user
     # If matching token is not found then AccessError is raised
@@ -22,21 +30,86 @@ def channels_listall(token):
         raise AccessError("Invalid Token")
 
     # Constructing list of all channels
+    channel_list = []
+
+    for channel in data['channels']:
+        for member in channel['all_members']:
+            if member['u_id'] == current_user['u_id']:
+                channel_details = {
+                    'channel_id': channel['channel_id'],
+                    'name': channel['name']
+                }
+
+                channel_list.append(channel_details)
 
     return {
-        'channels': [
-        	{
-        		'channel_id': 1,
-        		'name': 'My Channel',
-        	}
-        ],
+        'channels': channel_list
+    }
+
+def channels_listall(token):
+    '''
+    ----channels_listall Documentation----
+    Parameters:
+    (token)
+
+    Return Type:
+    {channels}
+
+    Exceptions:
+    N/A
+
+    Description:
+    Provide a list of all channels (and 
+    their associated details)
+    '''
+
+    # Checking token validity and finding user that accessed listall
+    current_user = {}
+    # Loops through users until matching token is found
+    for user in data['users']:
+        if user['token'] == token:
+            current_user = user
+    # If matching token is not found then AccessError is raised
+    if current_user == {}:
+        raise AccessError("Invalid Token")
+
+    # Constructing list of all channels
+    channel_list = []
+
+    for channel in data['channels']:
+        channel_details = {
+            'channel_id': channel['channel_id'],
+            'name': channel['name']
+        }
+
+        channel_list.append(channel_details)
+
+    return {
+        'channels': channel_list
     }
 
 def channels_create(token, name, is_public):
+    '''
+    ----channels_create Documentation----
+    Parameters:
+    (token, name, is_public)
+
+    Return Type:
+    {channel_id}
+
+    Exceptions:
+    InputError when any of:
+    - Name is more than 20 characters long
+
+    Description:
+    Creates a new channel with that name 
+    that is either a public or private channel
+    '''
+
     # Checking token validity and finding user that created channel
     current_user = {}
     # Loops through users until matching token is found
-    for user in users:
+    for user in data['users']:
         if user['token'] == token:
             current_user = user
     # If matching token is not found then AccessError is raised
@@ -52,7 +125,7 @@ def channels_create(token, name, is_public):
     channel_id = 0
 
     if channels != []:
-        channel_id = channels[-1]['channel_id'] + 1
+        channel_id = data['channels'][-1]['channel_id'] + 1
 
     # data.py dictionary entry is constructed
     new_channel = {
@@ -77,7 +150,7 @@ def channels_create(token, name, is_public):
     }
 
     # Entry is added to data.py
-    channels.append(new_channel)
+    data['channels'].append(new_channel)
 
     return {
         'channel_id': channel_id,
