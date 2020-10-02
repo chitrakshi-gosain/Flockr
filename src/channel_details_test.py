412f00327906711b1baf735d4ae97acb3c2af804
Ahmet Karatas <z5311022@ad.unsw.edu.au>
2020-10-03 09:39:36 +1000

    adding channel_messages and channel_details test files to iteration1

---
+++ src/channel_details_test.py
@@ -0,0 +1,106 @@
+from auth import auth_register
+from channel import channel_details, channel_join, channel_invite
+from channels import channels_create
+import pytest
+from other import clear
+from error import InputError
+from error import AccessError
+
+def test_insufficient_parameters():
+    clear()
+    with pytest.raises(InputError):
+        channel_details(None, None)
+
+def test_user1_not_authorised():
+
+    clear()
+    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')
+    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)
+
+    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')                # Register user_1
+    with pytest.raises(AccessError):
+        channel_details(user1_credentials['token'], channel1_id['channel_id'])
+
+
+def test_channel_id_not_valid():
+    clear()
+    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register owner
+    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')                # Register user_1
+
+    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', True)
+
+    invalid_channel_id = -1 
+    with pytest.raises(InputError):
+        channel_details(owner_credentials['token'], invalid_channel_id)
+
+
+def test_token_invalid():
+    clear()
+
+    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register owner
+    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')                # Register user_1
+
+    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)
+
+    with pytest.raises(AccessError):
+        channel_details('incorrect_user1_token', channel1_id['channel_id'])
+
+def test_return_type():
+    clear()
+    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register user_1
+    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')                # Register user_1
+
+    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)                                 # create a public channel
+
+    # Invite two users to the channel                   
+    channel_invite(user1_credentials['token'], channel1_id['channel_id'], user1_credentials['u_id'])
+
+    owner = {'u_id': owner_credentials['u_id'], 'name_first': 'owner_firstname', 'name_last': 'owner_lastname'}
+
+    user1 = {'u_id': user1_credentials['u_id'], 'name_first': 'user1_firstname', 'name_last': 'user1_lastname'}
+    channel_contents = {'name': 'channel1', 'owner_members': [owner], 'all_members': [owner, user1]}
+
+    assert isinstance(channel_contents['name'], str)
+
+    assert isinstance(channel_contents['owner_members'], list)
+    assert isinstance(channel_contents['owner_members'][0], dict)
+    assert isinstance(channel_contents['owner_members'][0]['u_id'], int)
+    assert isinstance(channel_contents['owner_members'][0]['name_first'], str)
+    assert isinstance(channel_contents['owner_members'][0]['name_last'], str)
+
+    assert isinstance(channel_contents['all_members'], list)
+    assert isinstance(channel_contents['all_members'][0], dict)
+    assert isinstance(channel_contents['all_members'][0]['u_id'], int)
+    assert isinstance(channel_contents['all_members'][0]['name_first'], str)
+    assert isinstance(channel_contents['all_members'][0]['name_last'], str)
+
+
+def test_channel_details_case():
+
+    clear()
+    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register user_1
+    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')                # Register user_1
+
+    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)                                 # create a public channel
+
+    # Invite two users to the channel                   
+    channel_invite(user1_credentials['token'], channel1_id['channel_id'], user1_credentials['u_id'])
+
+    owner = {'u_id': 'owner_id', 'name_first': 'owner_firstname', 'name_last': 'owner_lastname'}
+    user1 = {'u_id': 'user1_id', 'name_first': 'user1_firstname', 'name_last': 'user1_lastname'}
+    channel_contents = {'name': 'channel1', 'owner_members': [owner], 'all_members': [owner, user1]}
+
+    assert channel_contents == channel_details(user1_credentials['token'], channel1_id['channel_id'])
+
+
+def test_channel_details_empty_channel():
+
+    clear()
+    #login the owner and create channel
+    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register user_1
+    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', True)
+
+    channel_leave(owner_credentials['token'], channel1_id['channel_id'])
+    channel_contents = {'name': 'channel1_name', 'owner_members': [], 'all_members': []}                                # is this how you would represent 
+    assert channel_contents == channel_details(owner_credentials['token'], channel1['channel_id'])                                              # no members?
+
