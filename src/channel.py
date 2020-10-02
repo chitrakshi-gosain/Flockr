ed4d536889e7929945569b91e69a26b7a0c756b2
Ahmet Karatas <z5311022@ad.unsw.edu.au>
2020-10-03 01:09:18 +1000

    added updated version of channel.py

--- src/channel.py
+++ src/channel.py
@@ -8,6 +8,8 @@ def channel_invite(token, channel_id, u_id):
 
     
 def channel_details(token, channel_id):
+    if None in {token, channel_id}:
+        raise InputError('Insufficient parameters given')
 
     is_token_valid(token)
     u_id = find_user_id(token)
@@ -20,7 +22,10 @@ def channel_details(token, channel_id):
     channel_contents.update({'all_members': channel_dict['all_members']})
     return channel_contents
 
+
 def channel_messages(token, channel_id, start):
+    if None in {token, channel_id, start}:
+        raise InputError('Insufficient parameters given')
 
     is_token_valid(token)
     u_id = find_user_id(token)
@@ -38,6 +43,9 @@ def channel_messages(token, channel_id, start):
 
     # start_index is the index of the dictionary where the messages start loading from.
     # If start = 0, start_index will be the index of the last added dictionary.
+    if len(channel_dict['messages']) == 0:
+        messages_history.update({'end': -1})
+
     start_index = (num_message - 1) - start       
     messages_list = []                                     
 
@@ -49,49 +57,8 @@ def channel_messages(token, channel_id, start):
             break
 
     messages_history.update({'messages': messages_list})
+
     return messages_history
-
-def find_user_id(token):                    
-    for user in data.data['users']:
-        if user['token'] == token:
-            u_id = user['u_id']
-            return u_id
-
-
-def is_token_valid(token):
-    valid_token = False
-    for user in data.data['users']:
-        if user['token'] == token:
-            valid_token = True
-
-    if not valid_token:                                                 # this wasn't raised in the spec but I think it's something to account for
-        raise AccessError['Invalid Token']
-
-def is_user_authorised(token, u_id):
-    u_id = find_user_id(token)
-
-    for channel in data.data['channels']:
-        for member in channel['all_members']:
-            if member['u_id'] == u_id:
-                user_authorised = True
-
-    if not user_authorised:
-        raise AccessError('Authorised user is not a member of channel with channel_id')
-
-def is_channel_valid(channel_id):
-
-    channel_dict = {}
-    channel_valid = False
-    for channel in data.data['channels']:
-        if channel['channel_id'] == channel_id:
-            channel_valid = True
-            channel_dict = channel
-            break
-            
-    if not channel_valid:
-        raise InputError('Channel ID is not a valid channel')
-
-    return channel_dict
 
 def channel_leave(token, channel_id):
     return {
@@ -108,3 +75,49 @@ def channel_addowner(token, channel_id, u_id):
 def channel_removeowner(token, channel_id, u_id):
     return {
     }
+
+
+
+# HELPER FUNCTIONS
+def find_user_id(token):                    
+    for user in data.data['users']:
+        if user['token'] == token:
+            u_id = user['u_id']
+            return u_id
+
+
+def is_token_valid(token):
+    valid_token = False
+    for user in data.data['users']:
+        if user['token'] == token:
+            valid_token = True
+
+    if not valid_token:                                                
+        raise AccessError('Invalid Token')
+
+def is_user_authorised(token, u_id):
+    u_id = find_user_id(token)
+
+    user_authorised = False
+    for channel in data.data['channels']:
+        for member in channel['all_members']:
+            if member['u_id'] == u_id:
+                user_authorised = True
+
+    if not user_authorised:
+        raise AccessError('User is not authorised')
+
+def is_channel_valid(channel_id):
+
+    channel_dict = {}
+    channel_valid = False
+    for channel in data.data['channels']:
+        if channel['channel_id'] == channel_id:
+            channel_valid = True
+            channel_dict = channel
+            break
+            
+    if not channel_valid:
+        raise InputError('Channel_id is not valid')
+
+    return channel_dict
