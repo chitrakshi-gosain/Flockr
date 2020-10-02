b70a30fcc098863aa55b37065f36a2def819c325
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-10-02 11:37:29 +1000

    Fixed user registration set up and imported appropriate errors from error.py

--- src/channels_listall_test.py
+++ src/channels_listall_test.py
@@ -2,7 +2,7 @@ from auth import auth_register, auth_logout
 from channel import channel_details
 from channels import channels_create, channels_listall, channels_list
 from other import clear
-import error
+from error import AccessError, InputError
 import pytest
 
 '''
@@ -28,19 +28,19 @@ def initialise_user_data():
 
     # Register users:
     # Descriptive test data
-    owner_details = auth_register('owner@email.com', 'owner_pass', 'owner_first', 'owner_last')
-    user1_details = auth_register('user1@email.com', 'user1_pass', 'user1_first', 'user1_last')
-    user2_details = auth_register('user2@email.com', 'user2_pass', 'user2_first', 'user2_last')
-    user3_details = auth_register('user3@email.com', 'user3_pass', 'user3_first', 'user3_last')
-    user4_details = auth_register('user4@email.com', 'user4_pass', 'user4_first', 'user4_last')
-    user5_details = auth_register('user5@email.com', 'user5_pass', 'user5_first', 'user5_last')
+    owner_details = auth_register('owner@email.com', 'Owner_pass1!', 'owner_first', 'owner_last')
+    user1_details = auth_register('user1@email.com', 'User1_pass!', 'user1_first', 'user1_last')
+    user2_details = auth_register('user2@email.com', 'User2_pass!', 'user2_first', 'user2_last')
+    user3_details = auth_register('user3@email.com', 'User3_pass!', 'user3_first', 'user3_last')
+    user4_details = auth_register('user4@email.com', 'User4_pass!', 'user4_first', 'user4_last')
+    user5_details = auth_register('user5@email.com', 'User5_pass!', 'user5_first', 'user5_last')
 
     # Realistic test data
-    john_details = auth_register('johnsmith@gmail.com', 'qwertyuiop', 'John', 'Smith')
-    jane_details = auth_register('janesmith@hotmail.com', 'mydateofbirth', 'Jane', 'Smith')
-    noah_details = auth_register('noah_navarro@yahoo.com', 'aP00RPassWord', 'Noah', 'Navarro')
-    ingrid_details = auth_register('ingrid.cline@gmail.com', '572o75630', 'Ingrid', 'Cline')
-    donald_details = auth_register('donaldrichards@gmail.com', 'kjdfg;h;;df', 'Donald', 'Richards')
+    john_details = auth_register('johnsmith@gmail.com', 'qweRt1uiop!', 'John', 'Smith')
+    jane_details = auth_register('janesmith@hotmail.com', 'm3yDate0fb!rth', 'Jane', 'Smith')
+    noah_details = auth_register('noah_navarro@yahoo.com', 'aP00RP&ssWord1', 'Noah', 'Navarro')
+    ingrid_details = auth_register('ingrid.cline@gmail.com', '572o7563O*', 'Ingrid', 'Cline')
+    donald_details = auth_register('donaldrichards@gmail.com', 'kjDf2g@h@@df', 'Donald', 'Richards')
 
     # Returns user data that is implementation dependent (id, token)
     return {
