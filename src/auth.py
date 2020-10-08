7055659f150d8447b6aa2ff7e4a32045bd1f8ebd
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2020-10-09 09:39:54 +1100

    removed trailing whitespaces, modified lines to fit in max chars for code and comments respectively

--- src/auth.py
+++ src/auth.py
@@ -1,19 +1,22 @@
+'''
+Created collaboratively by Wed15Team2 2020 T3
+Contributer - Chitrakshi Gosain
+
+Iteration 1
+'''
+
 import re
 import data
 from error import InputError, AccessError
 
-# Created collaboratively by Wed15Team2 2020 T3
-# Contributer - Chitrakshi Gosain
-
-# Iteration 1
-
 '''
-*********************************BASIC TEMPLATE*********************************
+****************************BASIC TEMPLATE******************************
 '''
 
 '''
 FUNCTIONS_IN_THIS FILE(PARAMETERS) return {RETURN_VALUES}:
--> auth_register(email, password, name_first, name_last) return {u_id, token}
+-> auth_register(email, password, name_first, name_last) return
+   {u_id, token}
 -> auth_login(email,password) return {u_id, token}
 -> auth_logout(token) return {is_sucess}
 '''
@@ -47,12 +50,12 @@ EXCEPTIONS
             -> insufficient parameters
             -> email entered is not a valid email
             -> email address is already being used by another user
-            -> password entered is less than 6 characters long or more than 32
-               characters long
-            -> name_first is not between 1 and 50 characters inclusively in
-               length
-            -> name_last is not between 1 and 50 characters inclusively in
-               length
+            -> password entered is less than 6 characters long or more
+               than 32 characters long
+            -> name_first is not between 1 and 50 characters inclusively
+               in length
+            -> name_last is not between 1 and 50 characters inclusively
+               in length
 '''
 
 '''
@@ -60,18 +63,19 @@ KEEP IN MIND:
 -> make one function to check if user is registered and use it for both
    re-registration check and registered before login check
 -> allow multiple session log-ins,
-   * for this make a data.data['valid_tokens'] dict in data.py, have tokens as
-   key, and value as u_id this way we can keep track of multiple logins very
-   easily, but dont do it now everyone will have to change implementation, do it
-   after we are done merging all branches once, so if anything ever goes wrong
-   we have A BACKUP. also, this aint imp for itr 1 so dont stress. :)
+   * for this make a data.data['valid_tokens'] dict in data.py, have
+   tokens as key, and value as u_id this way we can keep track of
+   multiple logins very easily, but dont do it now everyone will have to
+   change implementation, do it after we are done merging all branches
+   once, so if anything ever goes wrong we have A BACKUP. also, this
+   aint imp for itr 1 so dont stress. :)
 '''
 
 def auth_login(email, password):
     '''
     DESCRIPTION:
-    Given a registered user's email and password and generates a valid token for
-    the user to remain authenticated
+    Given a registered user's email and password and generates a valid
+    token for the user to remain authenticated
 
     PARAMETERS:
         -> email : email-id of user
@@ -84,16 +88,17 @@ def auth_login(email, password):
 
     # Checking for InputError(s):
     if None in {email, password}:
-        raise InputError('Insufficient parameters, please enter: email, password')
+        raise InputError(description='Insufficient parameters, please enter: \
+                         email, password')
 
     if check_if_valid_email(email) is False:
-        raise InputError('Email entered is not a valid email')
+        raise InputError(description='Email entered is not a valid email')
 
     if check_if_registered_user(email) is False:
-        raise InputError('Email entered does not belong to a user')
+        raise InputError(description='Email entered does not belong to a user')
 
     if check_password(email, password) is False:
-        raise InputError('Password is not correct')
+        raise InputError(description='Password is not correct')
 
     # Since there are no InputError(s), hence proceeding forward:
 
@@ -103,8 +108,8 @@ def auth_login(email, password):
     # updating token in data.data['users']
     store_generated_token(email, user_token)
 
-    # returning the dictionary with users' u_id, and token authenticated for
-    # their session
+    # returning the dictionary with users' u_id, and token authenticated
+    # for their session
     return {
         'u_id': get_user_id_from_email(email),
         'token': user_token
@@ -113,26 +118,29 @@ def auth_login(email, password):
 def auth_logout(token):
     '''
     DESCRIPTION:
-    Given an active token, invalidates the token to log the user out. If a valid
-    token is given, and the user is successfully logged out, it returns true,
-    otherwise false
+    Given an active token, invalidates the token to log the user out. If
+    a valid token is given, and the user is successfully logged out, it
+    returns true, otherwise false
 
     PARAMETERS:
         -> token : token of the authenticated user
 
     RETURN VALUES:
-        -> is_success : True if user is successfully logged out, otherwise False
+        -> is_success : True if user is successfully logged out,
+                        otherwise False
     '''
 
     # Checking for InputError:
     if token is None:
-        raise InputError('Insufficient parameters. Please enter: token')
+        raise InputError(description='Insufficient parameters. Please enter: \
+                        token')
 
     # Checking for AccessError:
     if check_token(token) is False:
-        raise AccessError('Token passed in is not a valid token')
+        raise AccessError(description='Token passed in is not a valid token')
 
-    # Since there is no InputError or AccessError, hence proceeding forward:
+    # Since there is no InputError or AccessError, hence proceeding
+    # forward:
 
     return {
         'is_success': invalidating_token(token)
@@ -141,14 +149,15 @@ def auth_logout(token):
 def auth_register(email, password, name_first, name_last):
     '''
     DESCRIPTION:
-    Given a user's first and last name, email address, and password, creates a
-    new account for them and returns a new token for authentication in their
-    session. A handle is generated that is the concatentation of a
-    lowercase-only first name and last name. If the concatenation is longer than
-    20 characters, it is cutoff at 20 characters. If the handle is already
-    taken, user's u_id is concatenated at the very end, incase this exceeds the
-    length of 20 characters, the last characters of handle string are adjusted
-    to accomodate the user's u_id
+    Given a user's first and last name, email address, and password,
+    creates a new account for them and returns a new token for
+    authentication in their session. A handle is generated that is the
+    concatentation of a lowercase-only first name and last name. If the
+    concatenation is longer than 20 characters, it is cutoff at 20
+    characters. If the handle is already taken, user's u_id is
+    concatenated at the very end, incase this exceeds the length of 20
+    characters, the last characters of handle string are adjusted to
+    accomodate the user's u_id
 
     PARAMETERS:
         -> email : email-id of user
@@ -163,22 +172,28 @@ def auth_register(email, password, name_first, name_last):
 
     # Checking for InputError(s):
     if None in {email, password, name_first, name_last}:
-        raise InputError('Insufficient parameters. Please enter: email, password, name_first, name_last')
+        raise InputError(description='Insufficient parameters. Please enter: \
+                        email, password, name_first, name_last')
 
     if check_if_valid_email(email) is False:
-        raise InputError('Email entered is not a valid email')
+        raise InputError(description='Email entered is not a valid email')
 
     if check_if_valid_password(password) is False:
-        raise InputError('Password entered is less than 6 characters long or more than 32 characters long or contains Non-ASCII characters')
-   
+        raise InputError(description='Password entered is less than 6 \
+                        characters long or more than 32 characters long or \
+                            contains Non-ASCII characters')
+
     if check_name_length_and_is_a_whitesapce(name_first) is False:
-        raise InputError('name_first is not between 1 and 50 characters inclusively in length or is a whitespace')
+        raise InputError(description='name_first is not between 1 and 50 \
+                        characters inclusively in length or is a whitespace')
 
     if check_name_length_and_is_a_whitesapce(name_last) is False:
-        raise InputError('name_last is not between 1 and 50 characters inclusively in length or is a whitespace')
+        raise InputError(description='name_last is not between 1 and 50 \
+                        characters inclusively in length or is a whitespace')
 
     if check_if_registered_user(email) is True:
-        raise InputError('Email address is already being used by another user')
+        raise InputError(description='Email address is already being used by \
+                        another user')
 
     # Since there are no InputError(s), hence proceeding forward:
 
@@ -200,19 +215,19 @@ def auth_register(email, password, name_first, name_last):
     # appending the data of new_user to data dictionary in data file
     data.data['users'].append(new_user)
 
-    # logging-in the new_user to get the authenticated token for their current
-    # session
-    user_login_credentials = auth_login(email, password)  
+    # logging-in the new_user to get the authenticated token for their
+    # current session
+    user_login_credentials = auth_login(email, password)
 
     return {
-        'u_id': get_user_id_from_email(email),
+        'u_id': user_login_credentials['u_id'],
         'token': user_login_credentials['token']
     }
 
 def check_if_valid_email(email):
     '''
-    Given the email of the user to be registeredd checks if it is a valid email
-    using a regex
+    Given the email of the user to be registeredd checks if it is a
+    valid email using a regex
     '''
 
     regex = r'^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w+$'
@@ -222,11 +237,11 @@ def check_if_valid_email(email):
 
 def check_if_valid_password(password):
     '''
-    Given the password of the user to be registered checks it's length is in
-    valid range and if it has printable ASCII characters only
+    Given the password of the user to be registered checks it's length
+    is in valid range and if it has printable ASCII characters only
     '''
 
-    if not 6<= len(password) <= 32:
+    if not 6 <= len(password) <= 32:
         return False
     if not password.isprintable():
         return False
@@ -234,8 +249,9 @@ def check_if_valid_password(password):
 
 def check_name_length_and_is_a_whitesapce(name_to_check):
     '''
-    Given the first or last name of the user to be registered checks if it's
-    length is in valid range and if it is not completely a whitespace
+    Given the first or last name of the user to be registered checks if
+    it's length is in valid range and if it is not completely a
+    whitespace
     '''
 
     if not 1 <= len(name_to_check) <= 50:
@@ -246,8 +262,8 @@ def check_name_length_and_is_a_whitesapce(name_to_check):
 
 def check_if_registered_user(email):
     '''
-    Given the email of a user to be registered checks if the email is already
-    being used by another user
+    Given the email of a user to be registered checks if the email is
+    already being used by another user
     '''
 
     for user in data.data['users']:
@@ -258,13 +274,13 @@ def check_if_registered_user(email):
 def get_user_id_from_email(email):
     '''
     Given the email of the user who is:
-    -> already registered: finds the user by their email in the database and
-                           returns their existing u_id
-    -> to be registered: counts the number of existing users and assigns the
-                         next number as u_id
+    -> already registered: finds the user by their email in the database
+                           and returns their existing u_id
+    -> to be registered: counts the number of existing users and assigns
+                         the next number as u_id
     and then returns it
     '''
-  
+
     user_count = -1
     for user in data.data['users']:
         if user['email'] == email:
@@ -275,21 +291,24 @@ def get_user_id_from_email(email):
 
 def generate_handle(name_first, name_last, email):
     '''
-    Given the first and last name of the user, a handle is generated that is
-    the concatentation of a lowercase-only first name and last name. If the
-    concatenation is longer than 20 characters, it is cutoff at 20 characters.
-    If the handle is already taken, user's u_id is concatenated at the very end,
-    incase this concatenation exceeds the length of 20 characters, the last
-    characters of handle string (which already belomgs to another user) are
-    adjusted to accomodate the user's u_id in the very end
+    Given the first and last name of the user, a handle is generated
+    that is the concatentation of a lowercase-only first name and last
+    name. If the concatenation is longer than 20 characters, it is
+    cutoff at 20 characters. If the handle is already taken, user's u_id
+    is concatenated at the very end, incase this concatenation exceeds
+    the length of 20 characters, the last characters of handle string
+    (which already belongs to another user) are adjusted to accomodate
+    the user's u_id in the very end
     '''
 
     concatenated_names = name_first.lower() + name_last.lower()
     handle_string = concatenated_names[:20]
     status = False
+
     for user_with_same_handle in data.data['users']:
         if user_with_same_handle['handle_str'] == handle_string:
             status = True
+
     if status is True:
         user_id = str(get_user_id_from_email(email))
         cut_handle_till = 20 - len(user_id)
@@ -298,12 +317,13 @@ def generate_handle(name_first, name_last, email):
 
 def check_password(email, password):
     '''
-    Given the password of the user while logging-in matches the password with
-    the database file which stores the password of user made while registerating
+    Given the password of the user while logging-in matches the password
+    with the database file which stores the password of user made while
+    registering
     '''
 
     for user in data.data['users']:
-        if user['email']  == email:
+        if user['email'] == email:
             if user['password'] == password:
                 return True
     return False
@@ -322,21 +342,21 @@ def check_token(token):
 # token, store it and then return it
 def store_generated_token(email, user_token):
     '''
-    Given the email of the user trying of log-in stores the authenticated token
-    in database as necessary which later results in the authentication of the
-    user for the particular session
+    Given the email of the user trying of log-in stores the
+    authenticated token in database as necessary which later results in
+    the authentication of the user for the particular session
     '''
 
     for user in data.data['users']:
         if user['email'] == email:
             user['token'] = user_token
 
-# or do it this way, find the user with matching token and relace the token with
-#  invalid token string
+# or do it this way, find the user with matching token and relace the
+# token with invalid token string
 def invalidating_token(token):
     '''
-    Given the token of authenticated user invalidates it which later leads to
-    unauthentication of the user i.e. logging-out the user
+    Given the token of authenticated user invalidates it which later
+    leads to unauthentication of the user i.e. logging-out the user
     '''
 
     for user in data.data['users']:
@@ -346,8 +366,8 @@ def invalidating_token(token):
 
 def check_if_first_user():
     '''
-    Chceks if the registering user is the first user of Flockr, this later leads
-    to giving the first user the admin of Flockr status
+    Chceks if the registering user is the first user of Flockr, this
+    later leads to giving the first user the admin of Flockr status
     '''
     if len(data.data['users']) == 0:
         return True
@@ -395,25 +415,25 @@ if __name__ == '__main__':
 # NOT NEEDED ATM:
 
 # def check_if_valid_password(password):
-#     allowed_special_symbols = ['!', '@', '#', '$', '%', '^', '&', '*']   
+#     allowed_special_symbols = ['!', '@', '#', '$', '%', '^', '&', '*']
 
 #     if not 6 <= len(password) <= 32:
 #         return False
 
-#     if not any(character.isdigit() for character in password): 
-#         return False 
-          
-#     if not any(character.isupper() for character in password): 
+#     if not any(character.isdigit() for character in password):
 #         return False
-          
-#     if not any(character.islower() for character in password): 
+
+#     if not any(character.isupper() for character in password):
 #         return False
-          
-#     if not any(character in allowed_special_symbols for character in password): 
+
+#     if not any(character.islower() for character in password):
+#         return False
+
+#     if not any(character in allowed_special_symbols for character in password):
 #         return False
 
 #     return True
 
 
-# code used to debug logout :     print(token)  
-#                                 print(check_token(token))
+# code used to debug logout :     print(token)
+#                                 print(check_token(token)
