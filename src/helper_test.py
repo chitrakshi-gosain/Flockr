ca0dea5934834ec83ab427ff6392156f8ef26b86
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-10-10 20:18:03 +1100

    Added helper_test stub tests

---
+++ src/helper_test.py
@@ -0,0 +1,42 @@
+from helper import *
+import data
+import auth
+import channel
+import channels
+import other
+
+def test_check_if_valid_email():
+    pass
+
+def test_check_if_valid_password():
+    pass
+
+def test_check_name_length_and_is_a_whitesapce():
+    pass
+
+def test_invalidating_token():
+    pass
+
+def test_get_channel_info():
+    pass
+
+def test_is_user_authorised():
+    pass
+
+def test_is_channel_owner():
+    pass
+
+def test_get_user_info():
+    pass
+
+def test_is_user_in_channel():
+    pass
+
+def test_generate_handle():
+    pass
+
+def test_check_password():
+    pass
+
+def store_generated_token():
+    pass
