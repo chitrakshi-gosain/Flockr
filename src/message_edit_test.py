b45ffaed3a9ac1d170d9606477c4ad83f3396b32
Joseph Knox <josephdixonknox@gmail.com>
2020-10-10 14:29:11 +1100

    Created stub test file for message_edit

---
+++ src/message_edit_test.py
@@ -0,0 +1,25 @@
+# Created collaboratively by Wed15Team2 2020 T3
+# Contributer - Joseph Knox
+
+# Iteration 2
+
+
+# basic test with no edge case or errors raised
+def test_message_edit_noerrors():
+    pass
+
+# test that message_edit deletes the message
+# if provided with an empty string
+def test_message_edit_emptystring():
+    pass
+
+# test that message_edit raises AccessError
+# if user (based on token) is not the sender of message with message ID message_id
+def test_message_edit_notsender():
+    pass
+
+# test that message_edit raises AccessError
+# if token is not authorised
+# i.e. user is not admin of the flockr or owner of the channel message is in
+def test_message_edit_notauth():
+    pass
