611deea4200bfff454833728924403f2fa2ae47d
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2020-10-10 19:42:33 +1100

    made a boolean change

--- src/auth.py
+++ src/auth.py
@@ -1,4 +1,4 @@
-'''
+l'''
 Created collaboratively by Wed15Team2 2020 T3
 Contributer - Chitrakshi Gosain
 
@@ -6,7 +6,6 @@ Iteration 1
 '''
 
 import data
-from other import clear
 from error import InputError, AccessError
 from helper import check_if_valid_email, check_if_valid_password, \
 check_name_length_and_is_a_whitesapce, invalidating_token, get_user_info, \
@@ -139,7 +138,7 @@ def auth_logout(token):
                         token')
 
     # Checking for AccessError:
-    if not get_user_info('token', token):
+    if get_user_info('token', token) is False:
         raise AccessError(description='Token passed in is not a valid token')
 
     # Since there is no InputError or AccessError, hence proceeding
