fde81d7da2458538d7538d348c04baffd7a4e12e
Joseph Knox <josephdixonknox@gmail.com>
2020-10-14 07:51:42 +1100

    wrote basic tests

--- src/user_profile_setname_test.py
+++ src/user_profile_setname_test.py
@@ -3,27 +3,146 @@
 
 # Iteration 2
 
+import pytest
+import helper
+import auth
+import user
+from error import InputError, AccessError
+from other import clear
 
 # basic test with no edge case or errors raised
 def test_user_profile_setname_noerrors():
+    '''
+    clear()
+
+    name_first_old = 'name_first_old'
+    name_last_old = 'name_last_old'
+
+    user_details = auth.auth_register("user@email.com", "user_pass", "name_first_old", "name_last_old")
+    token = user_details['token']
+    u_id = user_details['u_id']
+    
+    user_dict = user.user_profile(token, u_id)
+
+    assert user_dict['name_first'] == name_first_old
+    assert user_dict['name_last'] == name_last_old
+
+    name_first_new = 'name_first_new'
+    name_last_new = 'name_last_new'
+
+    user.user_profile_setname(token, name_first_new, name_last_new)
+
+    user_dict = user.user_profile(token, u_id)
+
+    assert user_dict['name_first'] == name_first_new
+    assert user_dict['name_last'] == name_last_new
+    '''
     pass
 
 # test that user_profile_setname raises InputError
 # if provided name_first is <1 characters in length
 def test_user_profile_setname_firstname_tooshort():
+    '''
+    clear()
+
+    name_first_old = 'name_first_old'
+    name_last_old = 'name_last_old'
+
+    user_details = auth.auth_register("user@email.com", "user_pass", "name_first_old", "name_last_old")
+    token = user_details['token']
+    u_id = user_details['u_id']
+    
+    user_dict = user.user_profile(token, u_id)
+
+    assert user_dict['name_first'] == name_first_old
+    assert user_dict['name_last'] == name_last_old
+
+    # new name_first <1 characters in length
+    name_first_new = ''
+    name_last_new = 'name_last_new'
+
+    with pytest.raises(InputError)
+        user.user_profile_setname(token, name_first_new, name_last_new)
+    '''
     pass
 
 # test that user_profile_setname raises InputError
 # if provided name_first is >50 characters in length
 def test_user_profile_setname_firstname_toolong():
+    '''
+    clear()
+
+    name_first_old = 'name_first_old'
+    name_last_old = 'name_last_old'
+
+    user_details = auth.auth_register("user@email.com", "user_pass", "name_first_old", "name_last_old")
+    token = user_details['token']
+    u_id = user_details['u_id']
+    
+    user_dict = user.user_profile(token, u_id)
+
+    assert user_dict['name_first'] == name_first_old
+    assert user_dict['name_last'] == name_last_old
+
+    # new name_first >50 characters in length
+    name_first_new = '123456789012345678901234567890123456789012345678901'
+    name_last_new = 'name_last_new'
+
+    with pytest.raises(InputError)
+        user.user_profile_setname(token, name_first_new, name_last_new)
+    '''
     pass
 
 # test that user_profile_setname raises InputError
 # if provided name_last is <1 characters in length
 def test_user_profile_setname_lastname_tooshort():
+    '''
+    clear()
+
+    name_first_old = 'name_first_old'
+    name_last_old = 'name_last_old'
+
+    user_details = auth.auth_register("user@email.com", "user_pass", "name_first_old", "name_last_old")
+    token = user_details['token']
+    u_id = user_details['u_id']
+    
+    user_dict = user.user_profile(token, u_id)
+
+    assert user_dict['name_first'] == name_first_old
+    assert user_dict['name_last'] == name_last_old
+
+    # new name_last <1 characters in length
+    name_first_new = 'name_first_new'
+    name_last_new = ''
+
+    with pytest.raises(InputError)
+        user.user_profile_setname(token, name_first_new, name_last_new)
+    '''
     pass
 
 # test that user_profile_setname raises InputError
 # if provided name_last is >50 characters in length
 def test_user_profile_setname_lastname_toolong():
+    '''
+    clear()
+    
+    name_first_old = 'name_first_old'
+    name_last_old = 'name_last_old'
+
+    user_details = auth.auth_register("user@email.com", "user_pass", "name_first_old", "name_last_old")
+    token = user_details['token']
+    u_id = user_details['u_id']
+    
+    user_dict = user.user_profile(token, u_id)
+
+    assert user_dict['name_first'] == name_first_old
+    assert user_dict['name_last'] == name_last_old
+
+    # new name_last >50 characters in length
+    name_first_new = 'name_first_new'
+    name_last_new = '123456789012345678901234567890123456789012345678901'
+
+    with pytest.raises(InputError)
+        user.user_profile_setname(token, name_first_new, name_last_new)
+    '''
     pass
