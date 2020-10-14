870bcefb0bc455c6ef074f20dad79913bcc122eb
Chitrakshi Gosain <c.gosain@student.unsw.edu.au>
2020-10-14 17:02:58 +1100

    Some tests were added by Jordan

--- src/helper_test.py
+++ src/helper_test.py
@@ -14,6 +14,105 @@ import channel
 import channels
 import other
 
+def initialise_data():
+    data.data = {
+        'users': [
+            { # user -> u_id : 0
+                'u_id' : 0,
+                'is_admin' : True,
+                'email' : 'blah0@domain',
+                'name_first' : 'fname0',
+                'name_last' : 'lname0',
+                'handle_str' : 'fname0lname0',
+                'token' : 'blah0@domain',
+                'password' : 'password0'
+            },
+            { # user -> u_id : 1
+                'u_id' : 1,
+                'is_admin' : False,
+                'email' : 'blah1@domain',
+                'name_first' : 'fname1',
+                'name_last' : 'lname1',
+                'handle_str' : 'fname1lname1',
+                'token' : 'blah1@domain',
+                'password' : 'password1'
+            }
+        ],
+
+        'channels' : [
+            { # channels -> channel_id : 0
+                'channel_id' : 0,
+                'name' : 'ch_name0',
+                'is_public' : True,
+                'owner_members' : [ # owner_memebers of channel_id : 0
+                    {
+                        'u_id' : 0,
+                        'name_first' : 'fname0',
+                        'name_last' : 'fname1',
+                    }
+                ],
+                'all_members' : [ # all_members of channel_id : 0
+                    {
+                        'u_id' : 0,
+                        'name_first' : 'fname0',
+                        'name_last' : 'fname0',
+                    },
+                    {
+                        'u_id' : 1,
+                        'name_first' : 'fname1',
+                        'name_last' : 'fname1',
+                    }
+                ],
+                'messages' : [ # messages of channel_id : 0
+                    { # mesages -> message_id : 0
+                        'message_id' : 0,
+                        'u_id' : 0,
+                        'message' : 'messagecontents0',
+                        'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)' #(not in quotes)
+                    },
+                    { # mesages -> message_id : 1
+                        'message_id' : 1,
+                        'u_id' : 0,
+                        'message' : 'messagecontents1',
+                        'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)' #(not in quotes)
+                    }
+                ]
+            },
+            { # channels -> channel_id : 1
+                'channel_id' : 1,
+                'name' : 'ch_name1',
+                'is_public' : False,
+                'owner_members' : [ # owner_memebers of channel_id : 1
+                    {
+                        'u_id' : 1,
+                        'name_first' : 'fname1',
+                        'name_last' : 'fname1',
+                    }
+                ],
+                'all_members' : [# all_memebers of channel_id : 1
+                    {
+                        'u_id' : 0,
+                        'name_first' : 'fname0',
+                        'name_last' : 'fname0',
+                    },
+                    {
+                        'u_id' : 1,
+                        'name_first' : 'fname1',
+                        'name_last' : 'fname1',
+                    }
+                ],
+                'messages' : [ # messages of channel_id : 1
+                    { # mesages -> message_id : 0
+                        'message_id' : 0,
+                        'u_id' : 0,
+                        'message' : 'messagecontents0',
+                        'timecreated' : 'datetime(YYYY, MM, DD, HH, MM)' #(not in quotes)
+                    }
+                ]
+            }
+        ]
+    }
+
 def test_check_if_valid_email():
     pass
 
@@ -27,7 +126,12 @@ def test_invalidating_token():
     pass
 
 def test_get_channel_info():
-    pass
+    other.clear()
+    initialise_data()
+
+    assert get_channel_info(-1) == False
+    assert get_channel_info(0) == data.data['channels'][0]
+    assert get_channel_info(1) == data.data['channels'][1]
 
 def test_is_user_authorised():
     pass
@@ -36,13 +140,25 @@ def test_is_channel_owner():
     pass
 
 def test_get_user_info():
-    pass
+    other.clear()
+    initialise_data()
+
+    assert get_user_info('u_id', -1) == False
+    assert get_user_info('token', ' ') == False
+    assert get_user_info('email', ' ') == False
+
+    assert get_user_info('u_id', 0) == data.data['users'][0]
+    assert get_user_info('token', 'blah1@domain') == data.data['users'][1]
+    assert get_user_info('email', 'blah1@domain') == data.data['users'][1]
 
 def test_is_user_in_channel():
-    pass
+    other.clear()
+    initialise_data()
 
-def test_generate_handle():
-    pass
+    assert is_user_in_channel(0, 0) == True
+    assert is_user_in_channel(0, 1) == True
+    assert is_user_in_channel(1, 0) == True
+    assert is_user_in_channel(1, 1) == True
 
 def test_check_password():
     pass
