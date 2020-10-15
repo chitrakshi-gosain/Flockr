5e2eef9c6e454a3cbbb59ddf9d5d0513bdec9c46
Joseph Knox <josephdixonknox@gmail.com>
2020-10-15 21:38:11 +1100

    Added user_profile from Cyrus's branch. user_profile_setname passes all tests.

--- src/user.py
+++ src/user.py
@@ -46,13 +46,40 @@ EXCEPTIONS
 '''
 
 def user_profile(token, u_id):
+    '''
+    DESCRIPTION:
+    For a valid user, returns information about
+    their user_id, email, first name, last name,
+    and handle
+
+    PARAMETERS:
+        -> token, u_id
+
+    RETURN VALUES:
+        -> {user}
+
+    EXCEPTIONS:
+        -> AccessError: Invalid token
+        -> InputError: User with u_id is not a valid user
+    '''
+
+    # Checking token validity
+    user = helper.get_user_info('token', token)
+    if not user:
+        raise AccessError('Invalid Token')
+
+    # Checking u_id validity and getting user data
+    user = helper.get_user_info('u_id', u_id)
+    if not user:
+        raise InputError('Not a valid user')
+
     return {
         'user': {
-        	'u_id': 1,
-        	'email': 'cs1531@cse.unsw.edu.au',
-        	'name_first': 'Hayden',
-        	'name_last': 'Jacobs',
-        	'handle_str': 'hjacobs',
+        	'u_id': u_id,
+        	'email': user['email'],
+        	'name_first': user['name_first'],
+        	'name_last': user['name_last'],
+        	'handle_str': user['handle_str'],
         },
     }
 
