8281945e9e7a4967746352b1922d63ce365010ea
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-10-20 16:17:00 +1100

    Added pytest fixtures to channels_list_test

--- src/logic_tests/channels_list_test.py
+++ src/logic_tests/channels_list_test.py
@@ -43,7 +43,8 @@ their associated details) that the
 authorised user is part of
 '''
 
-def initialise_user_data():
+@pytest.fixture
+def users():
     '''
     Sets up various user sample data for testing purposes
     '''
@@ -83,12 +84,10 @@ def initialise_user_data():
     }
 
 
-def test_channels_list_valid_single():
+def test_channels_list_valid_single(users):
     '''
     Listing a single created channel
     '''
-
-    users = initialise_user_data()
 
     # Creating a basic public channel
     channel_id = channels_create(users['owner']['token'], 'A Basic Channel', True)
@@ -106,12 +105,10 @@ def test_channels_list_valid_single():
     clear()
 
 
-def test_channels_list_valid_same():
+def test_channels_list_valid_same(users):
     '''
     Listing multiple created channels from the same user
     '''
-
-    users = initialise_user_data()
 
     # Creating channels and storing ids
     channel_id = []
@@ -142,12 +139,10 @@ def test_channels_list_valid_same():
     clear()
 
 
-def test_channels_list_valid_different():
+def test_channels_list_valid_different(users):
     '''
     Listing multiple created channels from different users
     '''
-
-    users = initialise_user_data()
 
     # Creating channels and storing ids
     channel_id = []
@@ -182,12 +177,10 @@ def test_channels_list_valid_different():
     clear()
 
 
-def test_channels_list_valid_private():
+def test_channels_list_valid_private(users):
     '''
     Listing multiple created private channels from different users
     '''
-
-    users = initialise_user_data()
 
     # Creating channels and storing ids
     channel_id = []
@@ -219,12 +212,10 @@ def test_channels_list_valid_private():
     clear()
 
 
-def test_channels_list_valid_mix():
+def test_channels_list_valid_mix(users):
     '''
     Listing a mix of multiple public and private channels from different users with some sharing names
     '''
-
-    users = initialise_user_data()
 
     # Creating channels and storing ids
     channel_id = []
@@ -270,12 +261,10 @@ def test_channels_list_valid_mix():
     clear()
 
 
-def test_channels_list_valid_empty():
+def test_channels_list_valid_empty(users):
     '''
     Listing channels when none have been created
     '''
-
-    users = initialise_user_data()
 
     # Checking channels_list return is correct
     assert channels_list(users['user1']['token']) == {'channels': []}
@@ -283,12 +272,10 @@ def test_channels_list_valid_empty():
     clear()
 
 
-def test_channels_list_invalid_token():
+def test_channels_list_invalid_token(users):
     '''
     Attempting to call channels_list without a valid token
     '''
-
-    users = initialise_user_data()
 
     # Only way to guarrantee a token is invalid is to invalidate an existing token
     invalid_token = users['owner']['token']
