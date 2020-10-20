fff1f74d625dc5c46b146ee517c4bc6bcedcb9a2
Joseph Knox <josephdixonknox@gmail.com>
2020-10-21 10:28:39 +1100

    added fixtures to helper_test

--- src/logic_tests/helper_test.py
+++ src/logic_tests/helper_test.py
@@ -3,10 +3,11 @@ Created collaboratively by Wed15Team2 2020 T3
 Contributors - Jordan Hunyh, Chitrakshi Gosain, Cyrus Wilkie,
                Ahmet Karatas, Joseph Knox
 
-Iteration 1
+Iteration 2
 '''
 
 import unittest
+import pytest
 from helper import *
 import data
 import other
@@ -14,8 +15,25 @@ import other
 '''
 ****************************BASIC TEMPLATE******************************
 '''
+'''
+ADD TEMPLATE DOC HERE
+'''
 
+@pytest.fixture
+def reset():
+    '''
+    Resets the internal data of the application to it's initial state
+    '''
+
+    other.clear()
+
+@pytest.fixture
 def initialise_data():
+    '''
+    Sets up various descriptive user sample data for testing
+    purposes and returns user data which is implementation dependent
+    '''
+    
     data.data = {
         'users': [
             { # user -> u_id : 0
@@ -126,10 +144,49 @@ def initialise_data():
         ]
     }
 
+def test_check_if_valid_email(reset, initialise_data):
+    '''
+    Tests the email of the user to be registered checks is a
+    valid email using a regex
+    '''
 
+    # Invalid Cases
+    assert not check_if_valid_email('blatantlywrong')
+    assert not check_if_valid_email('vaguelytrying@')
+    assert not check_if_valid_email('abitcloser@gmail')
+    assert not check_if_valid_email('@ozemail.com.au')
+    assert not check_if_valid_email('')
 
+    # Valid Cases
+    assert check_if_valid_email('ingridcline@gmail.com')
+    assert check_if_valid_email('myemail@email.com')
+    assert check_if_valid_email('hi@hello.com')
 
-def test_check_if_valid_email():
+def test_check_if_valid_password(reset, initialise_data):
+    '''
+    Tests the password of the user to be registered checks it's length
+    is in valid range and if it has printable ASCII characters only
+    '''
+
+    # Invalid Cases
+    assert not check_if_valid_password('')
+    assert not check_if_valid_password('hello')
+    assert not check_if_valid_password('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161\
+        e5c1fa7425e73043362938b9824helloworld')
+    assert not check_if_valid_password('hello\nworld')
+
+    # Valid Cases
+    assert check_if_valid_password('mysecuredateofbirth')
+    assert check_if_valid_password('1234567890') 
+    assert check_if_valid_password('another valid pasword') 
+    assert check_if_valid_password('*&%&%*)^*#$') 
+
+def test_check_string_length_and_whitespace(reset, initialise_data):
+    '''
+    Tests name_first(1-50 char limit), name_last(1-50 char limit) and
+    handle_str(3-20 char limit), all char limits are inclusive
+    '''
+
     #Out of length range
     assert not check_string_length_and_whitespace(6,32,'123')
     assert not check_string_length_and_whitespace(6,32,'0123456789012345678901234567890123')
@@ -139,51 +196,20 @@ def test_check_if_valid_email():
     #Spaces
     assert not check_string_length_and_whitespace(5,20,'        ')
 
-def test_check_if_valid_password():
+
+def test_get_channel_info(reset, initialise_data):
     '''
-    Given the password of the user to be registered checks it's length
-    is in valid range and if it has printable ASCII characters only
+    ADD DOCSTRING HERE
     '''
-    # Invalid Cases
-    assert check_if_valid_password('')
-    assert check_if_valid_password('hello')
-    assert check_if_valid_password('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c\
-        1fa7425e73043362938b9824helloworld')
-    assert check_if_valid_password('hello\nworld')
-
-    # Valid Cases
-    assert check_if_valid_password('mysecuredateofbirth')
-    assert check_if_valid_password('1234567890') 
-    assert check_if_valid_password('another valid pasword') 
-    assert check_if_valid_password('*&%&%*)^*#$') 
-
-def test_check_string_length_and_whitespace():
-    # check name_first(1-50 char limit), name_last(1-50 char limit) and
-    # handle_str(3-20 char limit), all char limits are inclusive
-
-    #Out of length range
-    assert not check_string_length_and_whitespace(6,32,'123')
-    assert not check_string_length_and_whitespace(6,32,'012345678901234567890\
-        1234567890123')
-    #Edge
-    assert check_string_length_and_whitespace(6,32,'123456')
-    assert check_string_length_and_whitespace(6,32,'0123456789012345678901234\
-        5678901')
-    #Spaces
-    assert not check_string_length_and_whitespace(5,20,'        ')
-
-
-def test_get_channel_info():
-    other.clear()
-    initialise_data()
 
     assert not get_channel_info(-1)
     assert get_channel_info(0) == data.data['channels'][0]
     assert get_channel_info(1) == data.data['channels'][1]
 
-def test_is_user_authorised():
-    other.clear()
-    initialise_data()
+def test_is_user_authorised(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
 
     user0 = get_user_info('u_id', 0)
     token0 = user0['token']
@@ -196,18 +222,20 @@ def test_is_user_authorised():
     assert not is_user_authorised(generate_encoded_token(token1), 0)
 
 
-def test_is_channel_owner():
-    other.clear()
-    initialise_data()
+def test_is_channel_owner(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
 
     assert is_channel_owner(0, 0)
     assert not is_channel_owner(0, 1)
     assert is_channel_owner(1, 1)
     assert not is_channel_owner(1, 0)
 
-def test_get_user_info():
-    other.clear()
-    initialise_data()
+def test_get_user_info(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
 
     assert not get_user_info('u_id', -1)
     assert not get_user_info('token', ' ')
@@ -218,18 +246,20 @@ def test_get_user_info():
         data.data['users'][0]
     assert get_user_info('email', 'blah1@domain') == data.data['users'][1]
 
-def test_is_user_in_channel():
-    other.clear()
-    initialise_data()
+def test_is_user_in_channel(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
 
     assert is_user_in_channel(0, 0)
     assert is_user_in_channel(0, 1)
-    assert is_user_in_channel(1, 1)
     assert is_user_in_channel(1, 0)
+    assert is_user_in_channel(1, 1)
 
-def test_get_message_info():
-    other.clear()
-    initialise_data()
+def test_get_message_info(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
 
     assert get_message_info(0) == {
         'message_id': 0,
@@ -243,22 +273,31 @@ def test_get_message_info():
         'message': 'messagecontents1',
         'timecreated': 'datetime(YYYY, MM, DD, HH, MM)'
     }
-    assert not get_message_info(2)
 
-def test_encrypt_same_password_with_hash():
+def test_encrypt_same_password_with_hash(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
+
     password = 'user0_pass1!'
     pass_hash = encrypt_password_with_hash(password)
     assert pass_hash == encrypt_password_with_hash(password)
 
-def test_encrypt_different_password_with_hash():
+def test_encrypt_different_password_with_hash(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
+
     password1 = 'user0_pass1'
     password2 = 'user1_pass1'
     pass_hash1 = encrypt_password_with_hash(password1)
     assert pass_hash1 != encrypt_password_with_hash(password2)
 
-def test_generate_encoded_token():
+def test_generate_encoded_token(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
 
-    other.clear()
     u_id0 = 0
     token0 = generate_encoded_token(u_id0)
 
@@ -271,14 +310,26 @@ def test_generate_encoded_token():
     assert {token0 : u_id0} in data.data['valid_tokens']
     assert {token1 : u_id1} in data.data['valid_tokens']
 
-def test_decode_encoded_token():
+def test_decode_encoded_token(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
+
     # Chitrakshi
     pass
 
-def test_update_data():
+def test_update_data(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
+
     # Cyrus
     pass
 
-def test_invalidating_token():
+def test_invalidating_token(reset, initialise_data):
+    '''
+    ADD DOCSTRING HERE
+    '''
+
     # Ahmet
     pass
