c7044c8edeba8f5d940a8433f2a0ccb9019f4243
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-10-22 16:09:43 +1100

    reconfigured channels/listall to work with conftest.py

--- src/http_tests/channels_listall_http_test.py
+++ src/http_tests/channels_listall_http_test.py
@@ -25,7 +25,7 @@ json.dumps({RETURN VALUE})
 FIXTURES_USED_FOR_THIS_TEST (available in src/http_tests/conftest.py)
 -> reset
 -> url
--> initialise_users
+-> initialise_user_data
 '''
 
 '''
@@ -34,120 +34,28 @@ Error type: AccessError
     -> Invalid token
 '''
 
-@pytest.fixture
-def initialise_users(url):
-    '''
-    Sets up various user sample data for testing purposes
-    '''
-
-    # Register users:
-    # Descriptive test data
-    owner_details = requests.post(f'{url}/auth/register', json={
-        'email': 'owner@email.com',
-        'password': 'Owner_pass1!',
-        'name_first': 'owner_first',
-        'name_last': 'owner_last',
-    }).json()
-    user1_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user1@email.com',
-        'password': 'User1_pass!',
-        'name_first': 'user1_first',
-        'name_last': 'user1_last',
-    }).json()
-    user2_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user2@email.com',
-        'password': 'User2_pass!',
-        'name_first': 'user2_first',
-        'name_last': 'user2_last',
-    }).json()
-    user3_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user3@email.com',
-        'password': 'User3_pass!',
-        'name_first': 'user3_first',
-        'name_last': 'user3_last',
-    }).json()
-    user4_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user4@email.com',
-        'password': 'User4_pass!',
-        'name_first': 'user4_first',
-        'name_last': 'user4_last',
-    }).json()
-    user5_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user5@email.com',
-        'password': 'User5_pass!',
-        'name_first': 'user5_first',
-        'name_last': 'user5_last',
-    }).json()
-
-    # Realistic test data
-    john_details = requests.post(f'{url}/auth/register', json={
-        'email': 'johnsmith@gmail.com',
-        'password': 'qweRt1uiop!',
-        'name_first': 'John',
-        'name_last': 'Smith',
-    }).json()
-    jane_details = requests.post(f'{url}/auth/register', json={
-        'email': 'janesmith@hotmail.com',
-        'password': 'm3yDate0fb!rth',
-        'name_first': 'Jane',
-        'name_last': 'Smith',
-    }).json()
-    noah_details = requests.post(f'{url}/auth/register', json={
-        'email': 'noah_navarro@yahoo.com',
-        'password': 'aP00RP&ssWord1',
-        'name_first': 'Noah',
-        'name_last': 'Navarro',
-    }).json()
-    ingrid_details = requests.post(f'{url}/auth/register', json={
-        'email': 'ingrid.cline@gmail.com',
-        'password': '572o7563O*',
-        'name_first': 'Ingrid',
-        'name_last': 'Cline',
-    }).json()
-    donald_details = requests.post(f'{url}/auth/register', json={
-        'email': 'donaldrichards@gmail.com',
-        'password': 'kjDf2g@h@@df',
-        'name_first': 'Donald',
-        'name_last': 'Richards',
-    }).json()
-
-    # Returns user data that is implementation dependent (id, token)
-    return {
-        'owner': owner_details,
-        'user1': user1_details,
-        'user2': user2_details,
-        'user3': user3_details,
-        'user4': user4_details,
-        'user5': user5_details,
-        'john': john_details,
-        'jane': jane_details,
-        'noah': noah_details,
-        'ingrid': ingrid_details,
-        'donald': donald_details
-    }
-
 def test_url(url):
     '''
     A simple sanity test to check that the server is set up properly
     '''
     assert url.startswith("http")
 
-def test_channels_listall_valid_single(url, initialise_users):
+def test_channels_listall_valid_single(url, initialise_user_data):
     '''
     Listing a single created channel
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating a basic public channel
     channel_id = requests.post(f'{url}/channels/create', json={
-        'token': users['owner']['token'],
+        'token': users['user0']['token'],
         'name': 'A Basic Channel',
         'is_public': True,
     }).json()
 
     # Checking channels_list return is correct
     channel_list = requests.get(f'{url}/channels/listall', params={
-        'token': users['owner']['token'],
+        'token': users['user0']['token'],
     }).json()
 
     assert channel_list['channels'][0]['channel_id'] == channel_id['channel_id']
@@ -157,11 +65,11 @@ def test_channels_listall_valid_single(url, initialise_users):
     assert isinstance(channel_list['channels'][0]['channel_id'], int)
     assert isinstance(channel_list['channels'][0]['name'], str)
 
-def test_channels_listall_valid_same(url, initialise_users):
+def test_channels_listall_valid_same(url, initialise_user_data):
     '''
     Listing multiple created channels from the same user
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating channels and storing ids
     channel_id = []
@@ -211,11 +119,11 @@ def test_channels_listall_valid_same(url, initialise_users):
     assert channel_list['channels'][3]['name'] == 'Chatter'
     assert channel_list['channels'][4]['name'] == '3rd Channel'
 
-def test_channels_listall_valid_different(url, initialise_users):
+def test_channels_listall_valid_different(url, initialise_user_data):
     '''
     Listing multiple created channels from different users
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating channels and storing ids
     channel_id = []
@@ -231,17 +139,17 @@ def test_channels_listall_valid_different(url, initialise_users):
         'is_public': True,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['donald']['token'],
+        'token': users['user10']['token'],
         'name': 'Discussion',
         'is_public': True,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['john']['token'],
+        'token': users['user6']['token'],
         'name': 'Chatter',
         'is_public': True,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['ingrid']['token'],
+        'token': users['user9']['token'],
         'name': '3rd Channel',
         'is_public': True,
     }).json())
@@ -265,11 +173,11 @@ def test_channels_listall_valid_different(url, initialise_users):
     assert channel_list['channels'][3]['name'] == 'Chatter'
     assert channel_list['channels'][4]['name'] == '3rd Channel'
 
-def test_channels_listall_valid_private(url, initialise_users):
+def test_channels_listall_valid_private(url, initialise_user_data):
     '''
     Listing multiple created private channels from different users
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating channels and storing ids
     channel_id = []
@@ -285,17 +193,17 @@ def test_channels_listall_valid_private(url, initialise_users):
         'is_public': False,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['donald']['token'],
+        'token': users['user10']['token'],
         'name': 'Discussion',
         'is_public': False,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['john']['token'],
+        'token': users['user6']['token'],
         'name': 'Chatter',
         'is_public': False,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['ingrid']['token'],
+        'token': users['user9']['token'],
         'name': '3rd Channel',
         'is_public': False,
     }).json())
@@ -319,11 +227,11 @@ def test_channels_listall_valid_private(url, initialise_users):
     assert channel_list['channels'][3]['name'] == 'Chatter'
     assert channel_list['channels'][4]['name'] == '3rd Channel'
 
-def test_channels_listall_valid_mix(url, initialise_users):
+def test_channels_listall_valid_mix(url, initialise_user_data):
     '''
     Listing a mix of multiple public and private channels from different users with some sharing names
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating channels and storing ids
     channel_id = []
@@ -339,17 +247,17 @@ def test_channels_listall_valid_mix(url, initialise_users):
         'is_public': True,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['donald']['token'],
+        'token': users['user10']['token'],
         'name': 'Discussion',
         'is_public': True,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['john']['token'],
+        'token': users['user6']['token'],
         'name': 'Chatter',
         'is_public': True,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['ingrid']['token'],
+        'token': users['user9']['token'],
         'name': '3rd Channel',
         'is_public': True,
     }).json())
@@ -364,7 +272,7 @@ def test_channels_listall_valid_mix(url, initialise_users):
         'is_public': False,
     }).json())
     channel_id.append(requests.post(f'{url}/channels/create', json={
-        'token': users['jane']['token'],
+        'token': users['user7']['token'],
         'name': 'Private',
         'is_public': False,
     }).json())
@@ -394,25 +302,26 @@ def test_channels_listall_valid_mix(url, initialise_users):
     assert channel_list['channels'][6]['name'] == 'Channel 2'
     assert channel_list['channels'][7]['name'] == 'Private'
 
-def test_channels_listall_valid_empty(url, initialise_users):
+def test_channels_listall_valid_empty(url, initialise_user_data):
     '''
     Listing channels when none have been created
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Checking channels_list return is correct
     assert requests.get(f'{url}/channels/listall', params={
         'token': users['user1']['token'],
     }).json() == {'channels': []}
 
-def test_channels_listall_invalid_token(url, initialise_users):
+def test_channels_listall_invalid_token(url, initialise_user_data):
     '''
     Attempting to call channels_listall without a valid token
     '''
-    users = initialise_users
+    users = init
+
 
     # Only way to guarrantee a token is invalid is to invalidate an existing token
-    invalid_token = users['owner']['token']
+    invalid_token = users['user0']['token']
     requests.post(f'{url}/auth/logout', json={
         'token': invalid_token,
     })
