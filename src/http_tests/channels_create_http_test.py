c7229b06e8ca2a776f188b27bd801c5558c6a2ce
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-10-22 15:35:10 +1100

    reconfigured channels/create tests to work with conftest fixtures

--- src/http_tests/channels_create_http_test.py
+++ src/http_tests/channels_create_http_test.py
@@ -37,114 +37,21 @@ Error type: AccessError
     -> Invalid token
 '''
 
-@pytest.fixture
-def initialise_users(url):
-    '''
-    Sets up various user sample data for testing purposes
-    '''
-
-    # Register users:
-    # Descriptive test data
-    owner_details = requests.post(f'{url}/auth/register', json={
-        'email': 'owner@email.com',
-        'password': 'Owner_pass1!',
-        'name_first': 'owner_first',
-        'name_last': 'owner_last',
-    }).json()
-    user1_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user1@email.com',
-        'password': 'User1_pass!',
-        'name_first': 'user1_first',
-        'name_last': 'user1_last',
-    }).json()
-    user2_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user2@email.com',
-        'password': 'User2_pass!',
-        'name_first': 'user2_first',
-        'name_last': 'user2_last',
-    }).json()
-    user3_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user3@email.com',
-        'password': 'User3_pass!',
-        'name_first': 'user3_first',
-        'name_last': 'user3_last',
-    }).json()
-    user4_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user4@email.com',
-        'password': 'User4_pass!',
-        'name_first': 'user4_first',
-        'name_last': 'user4_last',
-    }).json()
-    user5_details = requests.post(f'{url}/auth/register', json={
-        'email': 'user5@email.com',
-        'password': 'User5_pass!',
-        'name_first': 'user5_first',
-        'name_last': 'user5_last',
-    }).json()
-
-    # Realistic test data
-    john_details = requests.post(f'{url}/auth/register', json={
-        'email': 'johnsmith@gmail.com',
-        'password': 'qweRt1uiop!',
-        'name_first': 'John',
-        'name_last': 'Smith',
-    }).json()
-    jane_details = requests.post(f'{url}/auth/register', json={
-        'email': 'janesmith@hotmail.com',
-        'password': 'm3yDate0fb!rth',
-        'name_first': 'Jane',
-        'name_last': 'Smith',
-    }).json()
-    noah_details = requests.post(f'{url}/auth/register', json={
-        'email': 'noah_navarro@yahoo.com',
-        'password': 'aP00RP&ssWord1',
-        'name_first': 'Noah',
-        'name_last': 'Navarro',
-    }).json()
-    ingrid_details = requests.post(f'{url}/auth/register', json={
-        'email': 'ingrid.cline@gmail.com',
-        'password': '572o7563O*',
-        'name_first': 'Ingrid',
-        'name_last': 'Cline',
-    }).json()
-    donald_details = requests.post(f'{url}/auth/register', json={
-        'email': 'donaldrichards@gmail.com',
-        'password': 'kjDf2g@h@@df',
-        'name_first': 'Donald',
-        'name_last': 'Richards',
-    }).json()
-
-    # Returns user data that is implementation dependent (id, token)
-    return {
-        'owner': owner_details,
-        'user1': user1_details,
-        'user2': user2_details,
-        'user3': user3_details,
-        'user4': user4_details,
-        'user5': user5_details,
-        'john': john_details,
-        'jane': jane_details,
-        'noah': noah_details,
-        'ingrid': ingrid_details,
-        'donald': donald_details
-    }
-
-
 def test_url(url):
     '''
     A simple sanity test to check that the server is set up properly
     '''
     assert url.startswith("http")
 
-def test_channels_create_valid_basic(url, initialise_users):
+def test_channels_create_valid_basic(url, initialise_user_data):
     '''
     Creating channel with valid data
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating a basic public channel
     channel_id = requests.post(f'{url}/channels/create', json={
-        'token': users['owner']['token'],
+        'token': users['user0']['token'],
         'name': 'A Basic Channel',
         'is_public': True,
     }).json()
@@ -154,23 +61,23 @@ def test_channels_create_valid_basic(url, initialise_users):
 
     # Check that channel details have all been set correctly
     basic_channel_details = requests.get(f'{url}/channel/details', params={
-        'token': users['owner']['token'],
+        'token': users['user0']['token'],
         'channel_id': channel_id['channel_id'],
     }).json()
 
     assert basic_channel_details['name'] == 'A Basic Channel'
-    assert basic_channel_details['owner_members'][0]['u_id'] == users['owner']['u_id']
-    assert basic_channel_details['owner_members'][0]['name_first'] == 'owner_first'
-    assert basic_channel_details['owner_members'][0]['name_last'] == 'owner_last'
-    assert basic_channel_details['all_members'][0]['u_id'] == users['owner']['u_id']
-    assert basic_channel_details['all_members'][0]['name_first'] == 'owner_first'
-    assert basic_channel_details['all_members'][0]['name_last'] == 'owner_last'
+    assert basic_channel_details['owner_members'][0]['u_id'] == users['user0']['u_id']
+    assert basic_channel_details['owner_members'][0]['name_first'] == 'user0user6_firstst'
+    assert basic_channel_details['owner_members'][0]['name_last'] == 'user0user6_firstt'
+    assert basic_channel_details['all_members'][0]['u_id'] == users['user0']['u_id']
+    assert basic_channel_details['all_members'][0]['name_first'] == 'user0user6_firstst'
+    assert basic_channel_details['all_members'][0]['name_last'] == 'user0user6_firstt'
 
-def test_channels_create_valid_empty(url, initialise_users):
+def test_channels_create_valid_empty(url, initialise_user_data):
     '''
     Creating channel with empty string name
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating public channel with empty string as name
     channel_id = requests.post(f'{url}/channels/create', json={
@@ -196,11 +103,11 @@ def test_channels_create_valid_empty(url, initialise_users):
     assert empty_channel_details['all_members'][0]['name_first'] == 'user1_first'
     assert empty_channel_details['all_members'][0]['name_last'] == 'user1_last'
 
-def test_channels_create_valid_private(url, initialise_users):
+def test_channels_create_valid_private(url, initialise_user_data):
     '''
     Creating private channel
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating private channel
     channel_id = requests.post(f'{url}/channels/create', json={
@@ -219,12 +126,12 @@ def test_channels_create_valid_private(url, initialise_users):
     }).json()
 
     assert private_channel_details['name'] == 'Private Disc'
-    assert private_channel_details['owner_members'][0]['u_id'] == users['john']['u_id']
-    assert private_channel_details['owner_members'][0]['name_first'] == 'John'
-    assert private_channel_details['owner_members'][0]['name_last'] == 'Smith'
-    assert private_channel_details['all_members'][0]['u_id'] == users['john']['u_id']
-    assert private_channel_details['all_members'][0]['name_first'] == 'John'
-    assert private_channel_details['all_members'][0]['name_last'] == 'Smith'
+    assert private_channel_details['owner_members'][0]['u_id'] == users['user6']['u_id']
+    assert private_channel_details['owner_members'][0]['name_first'] == 'user6_first'
+    assert private_channel_details['owner_members'][0]['name_last'] == 'user6_last'
+    assert private_channel_details['all_members'][0]['u_id'] == users['user6']['u_id']
+    assert private_channel_details['all_members'][0]['name_first'] == 'user6_first'
+    assert private_channel_details['all_members'][0]['name_last'] == 'user6_last'
 
     # Ensure that channel is private by attempting join from non-member
     assert requests.post(f'{url}/channel/join', json={
@@ -232,11 +139,11 @@ def test_channels_create_valid_private(url, initialise_users):
         'channel_id': channel_id['channel_id'],
     }).status_code == 400
 
-def test_channels_create_invalid_namesize(url, initialise_users):
+def test_channels_create_invalid_namesize(url, initialise_user_data):
     '''
     Creating channel with too large of a name
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating public channel with namesize > 20 characters
     requests.post(f'{url}/channels/create', json={
@@ -252,11 +159,11 @@ def test_channels_create_invalid_namesize(url, initialise_users):
         'is_public': False,
     }).status_code == 400
 
-def test_channels_create_valid_samename(url, initialise_users):
+def test_channels_create_valid_samename(url, initialise_user_data):
     '''
     Creating two channels with the same name
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Creating public channels with the same name
     channel_id1 = requests.post(f'{url}/channels/create', json={
@@ -280,14 +187,14 @@ def test_channels_create_valid_samename(url, initialise_users):
     assert channel_list['channels'][0]['channel_id'] == channel_id1['channel_id']
     assert channel_list['channels'][1]['channel_id'] == channel_id2['channel_id']
 
-def test_channels_create_invalid_token(url, initialise_users):
+def test_channels_create_invalid_token(url, initialise_user_data):
     '''
     Attempting to call channels_listall without a valid token
     '''
-    users = initialise_users
+    users = initialise_user_data
 
     # Only way to guarrantee a token is invalid is to invalidate an existing token
-    invalid_token = users['owner']['token']
+    invalid_token = users['user0']['token']
     requests.post(f'{url}/auth/logout', json={
         'token': invalid_token,
     })
