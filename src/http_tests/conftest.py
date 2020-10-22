d591b5d805df8b7486105edf382ac6fc7effcf5f
Ahmet Karatas <z5311022@ad.unsw.edu.au>
2020-10-22 15:20:47 +1100

    adding a new fixture initialise_user_dictionary

--- src/http_tests/conftest.py
+++ src/http_tests/conftest.py
@@ -44,40 +44,87 @@ def reset(url):
     '''
     Resets the internal data of the application to it's initial state
     '''
-    requests.post(f"{url}/clear")
+
+    requests.delete(f"{url}/clear")
 
 @pytest.fixture
-def initialise_user_data(url, reset):
+def initialise_user_dictionary(reset):
     '''
-    Sets up various descriptive user sample data for testing
+    Creates dictionaries with descriptive user data for testing
     purposes and returns user data which is implementation dependent
     '''
 
-    admin_details = requests.post(f"{url}/auth/register", json={
+    admin = {
         'email': 'admin@email.com',
         'password': 'admin_pass1!',
         'name_first': 'admin_first',
         'name_last': 'admin_last'
-    }).json()
+    }
 
-    user1_details = requests.post(f"{url}/auth/register", json={
+    user0 = {
+        'email': 'user0@email.com',
+        'password': 'user0_pass1!',
+        'name_first': 'user0_FIRST',
+        'name_last': 'user0_LAST'
+    }
+
+    user1 = {
         'email': 'user1@email.com',
         'password': 'user1_pass1!',
-        'name_first': 'user1_first',
-        'name_last': 'user1_last'
-    }).json()
-
-    user2_details = requests.post(f"{url}/auth/register", json={
+        'name_first': 'user1_FIRST',
+        'name_last': 'user1_LAST'
+    }
+    
+    user2 = {
         'email': 'user2@email.com',
         'password': 'user2_pass1!',
-        'name_first': 'user2_first',
-        'name_last': 'user2_last'
-    }).json()
+        'name_first': 'user2_FIRST',
+        'name_last': 'user2_LAST'
+    }
+
+    user3 = {
+        'email': 'user3@email.com',
+        'password': 'user3_pass1!',
+        'name_first': 'user3_FIRST',
+        'name_last': 'user3_LAST'
+    }
+    
+    return {
+        'admin_dict': admin, #<-- I also added a missing comma here
+        'user0_dict': user0,
+        'user1_dict': user1,
+        'user2_dict': user2,
+        'user3_dict': user3
+    }
+
+@pytest.fixture
+def initialise_user_data(url, initialise_user_dictionary):
+    '''
+    Sets up various descriptive user sample data for testing
+    purposes and returns user data which is implementation dependent
+    '''
+    
+    admin = initialise_user_dictionary['admin_dict']
+    admin_details = requests.post(f"{url}/auth/register", json=admin).json()
+
+    user0 = initialise_user_dictionary['user0_dict']
+    user0_details = requests.post(f"{url}/auth/register", json=user0).json()
+
+    user1 = initialise_user_dictionary['user1_dict']
+    user1_details = requests.post(f"{url}/auth/register", json=user1).json()
+
+    user2 = initialise_user_dictionary['user2_dict']
+    user2_details = requests.post(f"{url}/auth/register", json=user2).json()
+
+    user3 = initialise_user_dictionary['user3_dict']
+    user3_details = requests.post(f"{url}/auth/register", json=user3).json()
 
     return {
         'admin': admin_details,
+        'user0': user0_details,
         'user1': user1_details,
-        'user2': user2_details
+        'user2': user2_details,
+        'user3': user3_deatils
     }
 
 @pytest.fixture
