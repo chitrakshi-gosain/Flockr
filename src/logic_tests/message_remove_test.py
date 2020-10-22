b4bae9d4db18f520d81a714847ccd6d53856c496
Ahmet Karatas <z5311022@ad.unsw.edu.au>
2020-10-22 17:52:04 +1100

    adding fixtures to message_remove_test.py and message_send_test.py

--- src/logic_tests/message_remove_test.py
+++ src/logic_tests/message_remove_test.py
@@ -6,50 +6,69 @@ from message import message_send, message_remove
 from other import clear, search
 from error import InputError, AccessError
 
+@pytest.fixture
+def reset():
+    clear()
+
+@pytest.fixture
+def initialise_user_data(reset):
+    owner_details = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')   
+    user1_details = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')      
+
+    return {
+        'owner': owner_details,
+        'user1': user1_details
+    }
+
+@pytest.fixture
+def initialise_channel_data(initialise_user_data):
+    owner_credentials = initialise_user_data['owner']
+    public_channel = channels_create(owner_credentials['token'], 'channel1_name', True)
+    private_channel = channels_create(owner_credentials['token'], 'channel1_name', False)
+    return {
+        'public': public_channel,
+        'private': private_channel
+    }
+
 def get_messages(admin_token):
     messages = search(admin_token, '')
     return messages
      
-def test_insufficient_parameters():
-    clear()
+def test_insufficient_parameters(reset):
     with pytest.raises(InputError):
         message_remove(None, None)
 
-def test_user_not_authorised():
-    clear()
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')
-    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)
+def test_user_not_authorised(initialise_user_data, initialise_channel_data):
+    owner_credentials = initialise_user_data['owner']
+    channel1_id = initialise_channel_data['private']
     message_id = message_send(owner_credentials['token'], channel1_id['channel_id'], "Sample message")
-
-    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')                # Register user_1
+    user1_credentials = initialise_user_data['user1']      
 
     with pytest.raises(AccessError):
         message_remove(user1_credentials['token'], message_id['message_id'])
 
 
-def test_token_invalid():
-    clear()
-
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register owner
-    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)
+def test_token_invalid(initialise_user_data, initialise_channel_data):
+    owner_credentials = initialise_user_data['owner']
+    channel1_id = initialise_channel_data['private']
     message_id = message_send(owner_credentials['token'], channel1_id['channel_id'], "Sample message")
 
     with pytest.raises(AccessError):
         message_remove('incorrect_user1_token', message_id['message_id'])
 
-def test_invalid_message_id():
-    clear()
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register owner
-    channels_create(owner_credentials['token'], 'channel1_name', False)
+def test_invalid_message_id(initialise_user_data, initialise_channel_data):
+    
+    owner_credentials = initialise_user_data['owner']
+    channel1_id = initialise_channel_data['private']
 
     incorrect_message_id = -1
     with pytest.raises(InputError):
         message_remove(owner_credentials['token'], incorrect_message_id)
 
-def test_empty():
-    clear()
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register user_1
-    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)                                 # create a public channel
+def test_empty(initialise_user_data, initialise_channel_data):
+    
+    owner_credentials = initialise_user_data['owner']
+    channel1_id = initialise_channel_data['private']                              # create a public channel
     message_id = message_send(owner_credentials['token'], channel1_id['channel_id'], "Sample message")
     message_remove(owner_credentials['token'], message_id['message_id'])
 
@@ -58,12 +77,13 @@ def test_empty():
 
     
 
-def test_multiple_dicts():
-    clear()
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register user_1
-
+def test_multiple_dicts(initialise_user_data, initialise_channel_data):
+    
+    owner_credentials = initialise_user_data['owner']
+    channel1_id = initialise_channel_data['private']                              # create a public channel
+    
     admin_token = owner_credentials['token']
-    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')
+    user1_credentials = initialise_user_data['user1']      
     
     channel1_id = channels_create(owner_credentials['token'], 'channel1_name', True)                                 # create a public channel
     channel_join(user1_credentials['token'], channel1_id['channel_id'])
--- src/logic_tests/message_send_test.py
+++ src/logic_tests/message_send_test.py
@@ -7,47 +7,63 @@ import pytest
 from other import clear, search
 from error import InputError, AccessError
 
+@pytest.fixture
+def reset():
+    clear()
+
+@pytest.fixture
+def initialise_user_data(reset):
+    owner_details = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')   
+    user1_details = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')      
+
+    return {
+        'owner': owner_details,
+        'user1': user1_details
+    }
+
+@pytest.fixture
+def initialise_channel_data(initialise_user_data):
+    owner_credentials = initialise_user_data['owner']
+    public_channel = channels_create(owner_credentials['token'], 'channel1_name', True)
+    private_channel = channels_create(owner_credentials['token'], 'channel1_name', False)
+    return {
+        'public': public_channel,
+        'private': private_channel
+    }
+
 def get_messages(admin_token):
     messages = search(admin_token, '')
     return messages
 
-def test_insufficient_parameters():
-    clear()
+def test_insufficient_parameters(reset):
     with pytest.raises(InputError):
         message_send(None, None, None)
 
-def test_user_not_authorised():
-    clear()
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')
-    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)
-
-    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')                # Register user_1
+def test_user_not_authorised(initialise_user_data, initialise_channel_data):
+    
+    channel1_id = initialise_channel_data['private']
+    user1_credentials = initialise_user_data['user1']      
     with pytest.raises(AccessError):
         message_send(user1_credentials['token'], channel1_id['channel_id'], "Sample message")
 
 
-def test_channel_id_not_valid():
-    clear()
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register owner
-
+def test_channel_id_not_valid(initialise_user_data):
+    owner_credentials = initialise_user_data['owner']
     invalid_channel_id = -1
     with pytest.raises(InputError):
         message_send(owner_credentials['token'], invalid_channel_id, "Sample message")
 
 
-def test_token_invalid():
-    clear()
+def test_token_invalid(initialise_channel_data):
 
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register owner
-    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)
+    channel1_id = initialise_channel_data['private']
     with pytest.raises(AccessError):
         message_send('incorrect_user1_token', channel1_id['channel_id'], "Sample message")
 
-def test_return_type():
-    clear()
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register user_1
-    channel1_id = channels_create(owner_credentials['token'], 'channel1_name', False)                                 # create a public channel
+def test_return_type(initialise_user_data, initialise_channel_data):
+    owner_credentials = initialise_user_data['owner']
 
+    channel1_id = initialise_channel_data['private']
     message_send(owner_credentials['token'], channel1_id['channel_id'], "Sample message")
 
     admin_token = owner_credentials['token']
@@ -59,11 +75,11 @@ def test_return_type():
     assert isinstance(messages['messages'][0]['message'], str)
     assert isinstance(messages['messages'][0]['message'], object)
 
-def test_sample():
-    clear()
-    owner_credentials = auth_register('owner@gmail.com', 'owner_pw', 'owner_firstname', 'owner_lastname')             # Register user_1
-    user1_credentials = auth_register('user1@gmail.com', 'user1_pw', 'user1_firstname', 'user1_lastname')             # Register user_1
-
+def test_sample(initialise_user_data, initialise_channel_data):
+    
+    channel1_id = initialise_channel_data['private']
+    owner_credentials = initialise_user_data['owner']
+    user1_credentials = initialise_user_data['user1']      
 
     channel1_id = channels_create(owner_credentials['token'], 'channel1_name', True)                                 # create a public channel
     channel_join(user1_credentials['token'], channel1_id['channel_id'])
