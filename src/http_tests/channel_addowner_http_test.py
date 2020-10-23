46ed0f697acf95adafb38107318439aa34c90e88
Joseph Knox <josephdixonknox@gmail.com>
2020-10-23 17:00:15 +1100

    implemented initialisation fixtures

--- src/http_tests/channel_addowner_http_test.py
+++ src/http_tests/channel_addowner_http_test.py
@@ -42,94 +42,62 @@ def test_url(url):
     '''
     assert url.startswith("http")
 
-def test_http_channel_addowner_noerrors(initialise_user_data, url):
+def test_http_channel_addowner_noerrors(initialise_channel_data, initialise_user_data, url):
     '''
     basic test with no edge case or errors raised
     '''
 
-    # user0 with u_id0 and token0 is the first to register, thus also admin of the flockr
-    user0 = {
-        'email': 'user0@email.com',
-        'password': 'user0_pass1!',
-        'name_first': 'user0_first',
-        'name_last': 'user0_last'
-    }
-    user0_details = requests.post(f"{url}/auth/register", json=user0).json()
-    token0 = user0_details['token']
+    # user 'admin' is the first to register, thus also admin of the flockr
+    admin = initialise_user_data['admin']
+    token_admin = admin['token']
 
-    # user1 with u_id1 and token1 is not admin
-    user1 = {
-        'email': 'user1@email.com',
-        'password': 'user1_pass1!',
-        'name_first': 'user1_first',
-        'name_last': 'user1_last'
-    }
-    user1_details = requests.post(f"{url}/auth/register", json=user1).json()
-    u_id1, token1 = user1_details['u_id'], user1_details['token']
+    # user 'user0' is not admin
+    user0 = initialise_user_data['user0']
+    u_id0, token0 = user0['u_id'], user0['token']
 
-    # token0 creates channel, therefore user0 is member and owner of that channel
-    channel_info = requests.post(f"{url}/channels/create", json={
-        'token': token0,
-        'name': 'ch_name0',
-        'is_public': True
-    }).json()
+    # 'admin_publ' is a channel created by the user 'admin', thus 'admin' is a member and owner
+    channel_info = initialise_channel_data['admin_publ']
     channel_id = channel_info['channel_id']
 
-    # token1 joins channel, therefore is a member but not an owner
+    # user0 joins channel, therefore is a member but not an owner
     requests.post(f"{url}/channel/join", json={
-        'token': token1,
+        'token': token0,
         'channel_id': channel_id
     })
-    assert not helper.is_channel_owner(u_id1, channel_id)
+    assert not helper.is_channel_owner(u_id0, channel_id)
 
-    # user0 adds user1 as owner
+    # admin adds user0 as owner
     requests.post(f"{url}/channel/addowner", json={
-        'token': token0,
+        'token': token_admin,
         'channel_id': channel_id,
-        'u_id': u_id1
+        'u_id': u_id0
     })
 
-    assert helper.is_channel_owner(u_id1, channel_id)
+    assert helper.is_channel_owner(u_id0, channel_id)
 
-def test_http_channel_addowner_invalidchannel(initialise_user_data, url):
+def test_http_channel_addowner_invalidchannel(initialise_channel_data, initialise_user_data, url):
     '''
     test that channel_addowner raises InputError if channel_id is not a valid channel_id
     '''
 
-    # user0 with u_id0 and token0 is the first to register, thus also admin of the flockr
-    user0 = {
-        'email': 'user0@email.com',
-        'password': 'user0_pass1!',
-        'name_first': 'user0_first',
-        'name_last': 'user0_last'
-    }
-    user0_details = requests.post(f"{url}/auth/register", json=user0).json()
-    token0 = user0_details['token']
+    # user 'admin' is the first to register, thus also admin of the flockr
+    admin = initialise_user_data['admin']
+    token_admin = admin['token']
 
-    # user1 with u_id1 and token1 is not admin
-    user1 = {
-        'email': 'user1@email.com',
-        'password': 'user1_pass1!',
-        'name_first': 'user1_first',
-        'name_last': 'user1_last'
-    }
-    user1_details = requests.post(f"{url}/auth/register", json=user1).json()
-    u_id1, token1 = user1_details['u_id'], user1_details['token']
+    # user 'user0' is not admin
+    user0 = initialise_user_data['user0']
+    u_id0, token0 = user0['u_id'], user0['token']
 
-    # token0 creates channel, therefore user0 is member and owner of that channel
-    channel_info = requests.post(f"{url}/channels/create", json={
-        'token': token0,
-        'name': 'ch_name0',
-        'is_public': True
-    }).json()
+    # 'admin_publ' is a channel created by the user 'admin', thus 'admin' is a member and owner
+    channel_info = initialise_channel_data['admin_publ']
     channel_id = channel_info['channel_id']
 
-    # token1 joins channel, therefore is a member but not an owner
+    # user0 joins channel, therefore is a member but not an owner
     requests.post(f"{url}/channel/join", json={
-        'token': token1,
+        'token': token0,
         'channel_id': channel_id
     })
-    assert not helper.is_channel_owner(u_id1, channel_id)
+    assert not helper.is_channel_owner(u_id0, channel_id)
 
     # assume -1 is not a valid channel_id
     channel_id = -1
@@ -137,151 +105,102 @@ def test_http_channel_addowner_invalidchannel(initialise_user_data, url):
     # assert that channel_addowner raises InputError
     with pytest.raises(InputError):
         requests.post(f"{url}/channel/addowner", json={
-            'token': token0,
+            'token': token_admin,
             'channel_id': channel_id,
-            'u_id': u_id1
+            'u_id': u_id0
         })
 
-def test_http_channel_addowner_alreadyowner(initialise_user_data, url):
+def test_http_channel_addowner_alreadyowner(initialise_channel_data, initialise_user_data, url):
     '''
     test that channel_addowner raises InputError
     if user with provided u_id is already an owner of the channel
     '''
 
-    # user0 with u_id0 and token0 is the first to register, thus also admin of the flockr
-    user0 = {
-        'email': 'user0@email.com',
-        'password': 'user0_pass1!',
-        'name_first': 'user0_first',
-        'name_last': 'user0_last'
-    }
-    user0_details = requests.post(f"{url}/auth/register", json=user0).json()
-    token0 = user0_details['token']
+    # user 'admin' is the first to register, thus also admin of the flockr
+    admin = initialise_user_data['admin']
+    token_admin = admin['token']
 
-    # user1 with u_id1 and token1 is not admin
-    user1 = {
-        'email': 'user1@email.com',
-        'password': 'user1_pass1!',
-        'name_first': 'user1_first',
-        'name_last': 'user1_last'
-    }
-    user1_details = requests.post(f"{url}/auth/register", json=user1).json()
-    u_id1, token1 = user1_details['u_id'], user1_details['token']
+    # user 'user0' is not admin
+    user0 = initialise_user_data['user0']
+    u_id0, token0 = user0['u_id'], user0['token']
 
-    # token0 creates channel, therefore user0 is member and owner of that channel
-    channel_info = requests.post(f"{url}/channels/create", json={
-        'token': token0,
-        'name': 'ch_name0',
-        'is_public': True
-    }).json()
+    # 'admin_publ' is a channel created by the user 'admin', thus 'admin' is a member and owner
+    channel_info = initialise_channel_data['admin_publ']
     channel_id = channel_info['channel_id']
 
-    # token1 joins channel, therefore is a member but not an owner
+    # user0 joins channel, therefore is a member but not an owner
     requests.post(f"{url}/channel/join", json={
-        'token': token1,
+        'token': token0,
         'channel_id': channel_id
     })
-    assert not helper.is_channel_owner(u_id1, channel_id)
+    assert not helper.is_channel_owner(u_id0, channel_id)
 
-    # user0 adds user1 as owner
+    # admin adds user0 as owner
     requests.post(f"{url}/channel/addowner", json={
-        'token': token0,
+        'token': token_admin,
         'channel_id': channel_id,
-        'u_id': u_id1
+        'u_id': u_id0
     })
 
-    assert helper.is_channel_owner(u_id1, channel_id)
+    assert helper.is_channel_owner(u_id0, channel_id)
 
     # attempt to add owner twice
     # assert that channel_addowner raises InputError
     with pytest.raises(InputError):
         requests.post(f"{url}/channel/addowner", json={
-            'token': token0,
+            'token': token_admin,
             'channel_id': channel_id,
-            'u_id': u_id1
+            'u_id': u_id0
         })
 
-def test_http_channel_addowner_authnotowner(initialise_user_data, url):
+def test_http_channel_addowner_authnotowner(initialise_channel_data, initialise_user_data, url):
     '''
     test that channel_addowner raises AccessError
     if the authorised user is not an owner of the channel
     '''
 
-    # user0 with u_id0 and token0 is the first to register, thus also admin of the flockr
-    user0 = {
-        'email': 'user0@email.com',
-        'password': 'user0_pass1!',
-        'name_first': 'user0_first',
-        'name_last': 'user0_last'
-    }
-    user0_details = requests.post(f"{url}/auth/register", json=user0).json()
-    token0 = user0_details['token']
-
-    # user1 with u_id1 and token1 is not admin
-    user1 = {
-        'email': 'user1@email.com',
-        'password': 'user1_pass1!',
-        'name_first': 'user1_first',
-        'name_last': 'user1_last'
-    }
-    user1_details = requests.post(f"{url}/auth/register", json=user1).json()
-    u_id1, token1 = user1_details['u_id'], user1_details['token']
-
-    # token0 creates channel, therefore user0 is member and owner of that channel
-    channel_info = requests.post(f"{url}/channels/create", json={
-        'token': token0,
-        'name': 'ch_name0',
-        'is_public': True
-    }).json()
+    # 'admin_publ' is a channel created by the user 'admin'
+    channel_info = initialise_channel_data['admin_publ']
     channel_id = channel_info['channel_id']
 
-    # token1 joins channel, therefore is a member but not an owner
+    # user 'user0' is not admin
+    user0 = initialise_user_data['user0']
+    u_id0, token0 = user0['u_id'], user0['token']
+
+    # user0 joins channel, therefore is a member but not an owner
     requests.post(f"{url}/channel/join", json={
-        'token': token1,
+        'token': token0,
         'channel_id': channel_id
     })
 
     # assert that channel_addowner raises AccessError
     with pytest.raises(AccessError):
         requests.post(f"{url}/channel/addowner", json={
-            'token': token1,
+            'token': token0,
             'channel_id': channel_id,
-            'u_id': u_id1
+            'u_id': u_id0
         })
 
-def test_http_channel_addowner_invalidtoken(initialise_user_data, url):
+def test_http_channel_addowner_invalidtoken(initialise_channel_data, initialise_user_data, url):
     '''
     test that channel_addowner raises AccessError if the provided token is not valid
     '''
 
-    user = {
-        'email': 'user@email.com',
-        'password': 'user_pass1!',
-        'name_first': 'user_first',
-        'name_last': 'user_last'
-    }
-    user_details = requests.post(f"{url}/auth/register", json=user).json()
-    u_id, token = user_details['u_id'], user_details['token']
+    # user 'admin' is the first to register, thus also admin of the flockr
+    admin = initialise_user_data['admin']
+    u_id_admin, token_admin = admin['u_id'], admin['token']
 
-    channel_info = requests.post(f"{url}/channels/create", json={
-        'token': token,
-        'name': 'ch_name2',
-        'is_public': True
-    }).json()
+    # 'admin_publ' is a channel created by the user 'admin', thus 'admin' is a member and owner
+    channel_info = initialise_channel_data['admin_publ']
     channel_id = channel_info['channel_id']
 
-    requests.post(f"{url}/channel/join", json={
-        'token': token,
-        'channel_id': channel_id
-    })
-
     # assume " " is always an invalid token
-    token = " "
+    token_admin = " "
 
     # assert that channel_addowner raises AccessError
     with pytest.raises(AccessError):
         requests.post(f"{url}/channel/addowner", json={
-            'token': token,
+            'token': token_admin,
             'channel_id': channel_id,
-            'u_id': u_id
+            'u_id': u_id_admin
         })
