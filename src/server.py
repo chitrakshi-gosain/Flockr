097691a5d222d5cdce95fa1bfef9990f1c40ba30
jordan <z5169771@ad.unsw.edu.au>
2020-10-23 20:17:20 +1100

    implemented tested functions

--- src/server.py
+++ src/server.py
@@ -12,12 +12,12 @@ from flask_cors import CORS
 from error import InputError
 # import sys
 from auth import auth_login, auth_register, auth_logout
-# from channel import channel_invite, channel_details, channel_messages, \
-#     channel_leave, channel_join, channel_addowner, channel_removeowner
-# from channels import channels_list, channels_listall, channels_create
-# from message import message_send, message_remove, message_edit
+from channel import channel_invite, channel_details, channel_messages, \
+     channel_leave, channel_join, channel_addowner, channel_removeowner
+from channels import channels_list, channels_listall, channels_create
+from message import message_send, message_remove, message_edit
 # from  user import user_profile, user_profile_setname, user_profile_setemail, \
-#     user_profile_sethandle  
+#     user_profile_sethandle
 from other import users_all, admin_userpermission_change, search, clear
 
 # need to plan how to write things here
@@ -47,7 +47,7 @@ def auth_login_route():
     '''
     DONT COMMIT THIS YET
     '''
-    
+
     payload = request.get_json()
     user_credentials = auth_login(payload['email'], payload['password'])
     return dumps({
@@ -81,19 +81,27 @@ def auth_register_route():
         'token': user_credentials['token']
     })
 
-# @APP.route("/channel/invite", methods=['POST'])
-# def channel_invite_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+@APP.route("/channel/invite", methods=['POST'])
+def channel_invite_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     payload = request.get_json()
+     token = payload['token']
+     channel_id = int(payload['channel_id'])
+     u_id = int(payload['u_id'])
 
-# @APP.route("/channel/details", methods=['GET'])
-# def channel_details_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+     return dumps(channel_invite(token, channel_id, u_id))
+
+@APP.route("/channel/details", methods=['GET'])
+def channel_details_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     token = request.args.get('token')
+     channel_id = int(request.args.get('channel_id'))
+
+     return dumps(channel_details(token, channel_id))
 
 # @APP.route("/channel/messages", methods=['GET'])
 # def channel_messages_route():
@@ -102,26 +110,39 @@ def auth_register_route():
 #     '''
 #     pass
 
-# @APP.route("/channel/leave", methods=['POST'])
-# def channel_leave_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+@APP.route("/channel/leave", methods=['POST'])
+def channel_leave_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     payload = request.get_json()
+     token = payload['token']
+     channel_id = int(payload['channel_id'])
 
-# @APP.route("/channel/join", methods=['POST'])
-# def channel_join_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+     return dumps(channel_leave(token, channel_id))
 
-# @APP.route("/channel/addowner", methods=['POST'])
-# def channel_addowner_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+@APP.route("/channel/join", methods=['POST'])
+def channel_join_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     payload = request.get_json()
+     token = payload['token']
+     channel_id = int(payload['channel_id'])
+
+     return dumps(channel_join(token, channel_id))
+
+@APP.route("/channel/addowner", methods=['POST'])
+def channel_addowner_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     payload = request.get_json()
+     token = payload['token']
+     channel_id = int(payload['channel_id'])
+     u_id = int(payload['u_id'])
+
+     return dumps(channel_addowner(token, channel_id, u_id))
 
 # @APP.route("/channel/removeowner", methods=['POST'])
 # def channel_removeowner_route():
@@ -144,19 +165,29 @@ def auth_register_route():
 #     '''
 #     pass
 
-# @APP.route("/channels/create", methods=['POST'])
-# def channels_create_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+@APP.route("/channels/create", methods=['POST'])
+def channels_create_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     payload = request.get_json()
+     token = payload['token']
+     name = payload['name']
+     is_public = bool(payload['is_public'])
 
-# @APP.route("/message/send", methods=['POST'])
-# def message_send_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+     return dumps(channels_create(token, name, is_public))
+
+@APP.route("/message/send", methods=['POST'])
+def message_send_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     payload = request.get_json()
+     token = payload['token']
+     channel_id = int(payload['channel_id'])
+     message_str = payload['message']
+
+     return dumps(message_send(token, channel_id, message_str))
 
 # @APP.route("/message/remove", methods=['DELETE'])
 # def message_remove_route():
@@ -207,19 +238,27 @@ def auth_register_route():
 #     '''
 #     pass
 
-# @APP.route("/admin/userpermission/change", methods=['POST'])
-# def change_userpermission_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+@APP.route("/admin/userpermission/change", methods=['POST'])
+def change_userpermission_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     payload = request.get_json()
+     token = payload['token']
+     u_id = int(payload['u_id'])
+     permission_id = int(payload['permission_id'])
 
-# @APP.route("/search", methods=['GET'])
-# def search_route():
-#     '''
-#     ADD DOCSTRING HERE
-#     '''
-#     pass
+     return dumps(admin_userpermission_change(token, u_id, permission_id))
+
+@APP.route("/search", methods=['GET'])
+def search_route():
+     '''
+     ADD DOCSTRING HERE (UNOFFICIAL)
+     '''
+     token = request.args.get('token')
+     query_str = request.args.get('query_str')
+
+     return dumps(search(token, query_str))
 
 @APP.route("/clear", methods=['DELETE'])
 def clear_route():
@@ -227,7 +266,7 @@ def clear_route():
     THIS IS NOT OFFICIAL, THIS IS JUST TO GET AUTH TESTS WORKING
     THIS FUNCTION IS OFFICIALLY IMPLEMENTED BY Jordan Hunyh
     '''
-    
+
     clear()
     return dumps({})
 
