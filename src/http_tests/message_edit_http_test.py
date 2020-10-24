4c5fe4a543cde101f204c76224dd7f3491e7adf9
Joseph Knox <josephdixonknox@gmail.com>
2020-10-24 17:12:06 +1100

    Filled out header templates

--- src/http_tests/message_edit_http_test.py
+++ src/http_tests/message_edit_http_test.py
@@ -8,8 +8,6 @@ Iteration 2
 import json
 import requests
 import pytest
-import helper
-from error import InputError, AccessError
 
 '''
 ****************************BASIC TEMPLATE******************************
@@ -18,22 +16,33 @@ from error import InputError, AccessError
 '''
 APP.routes_USED_fOR_THIS_TEST("/rule", methods=['METHOD']) return
 json.dumps({RETURN VALUE})
--> APP.route(.....) return json.dumps({...})
+-> APP.route("/auth/register", methods=['POST']) return
+   json.dumps({u_id, token})
+-> APP.route("/channels/create", methods=['POST']) return
+    json.dumps({channel_id})
+-> APP.route("/channel/join", methods=['POST']) return
+    json.dumps({})
+-> APP.route("/message/send", methods=['POST']) return
+    json.dumps({message_id})
+-> APP.route("/message/edit", methods=['PUT']) return
+    json.dumps({})
+-> APP.route("/search", methods=['GET']) return
+    json.dumps({messages})
 '''
 
 '''
 FIXTURES_USED_FOR_THIS_TEST (available in src/http_tests/conftest.py)
--> reset
 -> url
--> ...
+-> reset
+-> initialise_user_data
+-> initialise_channel_data
 '''
 
 '''
 EXCEPTIONS
-Error type: InputError
-    -> ..
 Error type: AccessError
-    -> ..
+    -> user (based on token) is not sender of message with message_id
+    -> token is not valid
 '''
 
 def test_url(url):
