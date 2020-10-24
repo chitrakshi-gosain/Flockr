3fe2bbcfa13c2dc8801725e4f9368948a0ab7cbf
Joseph Knox <josephdixonknox@gmail.com>
2020-10-24 12:47:20 +1100

    removed helper functions

--- src/logic_tests/channel_removeowner_test.py
+++ src/logic_tests/channel_removeowner_test.py
@@ -8,8 +8,8 @@ Iteration 1
 import pytest
 import auth
 from helper import is_channel_owner
-from channel import channel_addowner, channel_removeowner, channel_join
-from channels import channels_create
+from channel import channel_addowner, channel_removeowner, channel_join, channel_details
+from channels import channels_create, channels_listall
 from error import InputError, AccessError
 from other import clear
 
@@ -52,83 +52,150 @@ KEEP IN MIND:
 def test_channel_removeowner_noerrors():
     clear()
 
-    admin = auth.auth_register("admin@email.com", "admin_pass", "admin_first", "admin_last")
-    user0 = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    # user0 with u_id0 and token0 is the first to register, thus also admin of the flockr
+    user0_details = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    token0 = user0_details['token']
 
-    channel_info = channels_create(admin['token'], "ch_name2", True)
+    # user1 with u_id1 and token1 is not admin
+    user1_details = auth.auth_register("user1@email.com", "user1_pass", "user1_first", "user1_last")
+    u_id1, token1 = user1_details['u_id'], user1_details['token']
+
+    # token0 creates channel, therefore user0 is member and owner of that channel
+    channel_info = channels_create(token0, "ch_name0", True)
     channel_id = channel_info['channel_id']
-    channel_join(user0['token'], channel_id)
 
-    assert not is_channel_owner(user0['u_id'], channel_id)
-    channel_addowner(admin['token'], channel_id, user0['u_id'])
+    # token1 joins channel, therefore is a member but not an owner
+    channel_join(token1, channel_id)
 
-    assert is_channel_owner(user0['u_id'], channel_id)
-    channel_removeowner(admin['token'], channel_id, user0['u_id'])
-    assert not is_channel_owner(user0['u_id'], channel_id)
+    # assert user with u_id1 is not an owner
+    channels_list = channels_listall(token0)['channels']
+    for channel in channels_list:
+        if channel['channel_id'] == channel_id:
+            channel_dict = channel_details(token0, channel_id)
+            assert not any(user['u_id'] == u_id1 for user in channel_dict['owner_members'])
+            break
 
+    # user0 adds user1 as owner
+    channel_addowner(token0, channel_id, u_id1)
+
+    # assert user with u_id1 is an owner
+    channels_list = channels_listall(token0)['channels']
+    for channel in channels_list:
+        if channel['channel_id'] == channel_id:
+            channel_dict = channel_details(token0, channel_id)
+            assert any(user['u_id'] == u_id1 for user in channel_dict['owner_members'])
+            break
+    
+    # user0 removes user1 as owner
+    channel_removeowner(token0, channel_id, u_id1)
+
+    # assert user with u_id1 is not an owner
+    channels_list = channels_listall(token0)['channels']
+    for channel in channels_list:
+        if channel['channel_id'] == channel_id:
+            channel_dict = channel_details(token0, channel_id)
+            assert not any(user['u_id'] == u_id1 for user in channel_dict['owner_members'])
+            break
 
 # test that channel_removeowner raises InputError if channel_id is not a valid channel_id
 def test_channel_removeowner_invalidchannel():
     clear()
 
-    admin = auth.auth_register("admin@email.com", "admin_pass", "admin_first", "admin_last")
-    user0 = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    # user0 with u_id0 and token0 is the first to register, thus also admin of the flockr
+    user0_details = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    token0 = user0_details['token']
 
-    channel_info = channels_create(admin['token'], "ch_name2", True)
+    # user1 with u_id1 and token1 is not admin
+    user1_details = auth.auth_register("user1@email.com", "user1_pass", "user1_first", "user1_last")
+    u_id1, token1 = user1_details['u_id'], user1_details['token']
+
+    # token0 creates channel, therefore user0 is member and owner of that channel
+    channel_info = channels_create(token0, "ch_name0", True)
     channel_id = channel_info['channel_id']
 
-    channel_join(user0['token'], channel_id)
+    # token1 joins channel, therefore is a member but not an owner
+    channel_join(token1, channel_id)
 
-    channel_addowner(admin['token'], channel_id, user0['u_id'])
+    # user0 adds user1 as owner
+    channel_addowner(token0, channel_id, u_id1)
 
     # assume -1 is not a valid channel id
-    invalid_channel_id = -1
+    channel_id = -1
 
-    # assert that channel_addowner raises InputError
+    # assert that channel_removeowner raises InputError
     with pytest.raises(InputError):
-        channel_removeowner(admin['token'], invalid_channel_id, user0['u_id'])
+        channel_removeowner(token0, channel_id, u_id1)
 
 # test that channel_removeowner raises InputError
 # if user with provided u_id is not an owner of the channel
 def test_channel_removeowner_notowner():
     clear()
 
-    admin = auth.auth_register("admin@email.com", "admin_pass", "admin_first", "admin_last")
-    user0 = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    # user0 with u_id0 and token0 is the first to register, thus also admin of the flockr
+    user0_details = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    token0 = user0_details['token']
 
-    channel_info = channels_create(admin['token'], "ch_name2", True)
+    # user1 with u_id1 and token1 is not admin
+    user1_details = auth.auth_register("user1@email.com", "user1_pass", "user1_first", "user1_last")
+    u_id1, token1 = user1_details['u_id'], user1_details['token']
+
+    # token0 creates channel, therefore user0 is member and owner of that channel
+    channel_info = channels_create(token0, "ch_name0", True)
     channel_id = channel_info['channel_id']
 
-    channel_join(user0['token'], channel_id)
+    # token1 joins channel, therefore is a member but not an owner
+    channel_join(token1, channel_id)
 
-    # add owner
-    channel_addowner(admin['token'], channel_id, user0['u_id'])
-    assert is_channel_owner(user0['u_id'], channel_id)
+    # user0 adds user1 as owner
+    channel_addowner(token0, channel_id, u_id1)
+
+    # assert user with u_id1 is an owner
+    channels_list = channels_listall(token0)['channels']
+    for channel in channels_list:
+        if channel['channel_id'] == channel_id:
+            channel_dict = channel_details(token0, channel_id)
+            assert any(user['u_id'] == u_id1 for user in channel_dict['owner_members'])
+            break
+
+    # user0 removes user1 as owner
+    channel_removeowner(token0, channel_id, u_id1)
+
+    # assert user with u_id1 is not an owner
+    channels_list = channels_listall(token0)['channels']
+    for channel in channels_list:
+        if channel['channel_id'] == channel_id:
+            channel_dict = channel_details(token0, channel_id)
+            assert not any(user['u_id'] == u_id1 for user in channel_dict['owner_members'])
+            break
 
     # attempt to remove owner twice
-    channel_removeowner(admin['token'], channel_id, user0['u_id'])
-    assert not is_channel_owner(user0['u_id'], channel_id)
-
     # assert that channel_removeowner raises InputError
     with pytest.raises(InputError):
-        channel_removeowner(admin['token'], channel_id, user0['u_id'])
+        channel_removeowner(token0, channel_id, u_id1)
 
 # test that channel_removeowner raises AccessError
 # if the authorised user is not an owner of the channel or admin of the flockr
 def test_channel_removeowner_authnotowner():
     clear()
 
-    admin = auth.auth_register("admin@email.com", "admin_pass", "admin_first", "admin_last")
-    user0 = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    # user0 with u_id0 and token0 is the first to register, thus also admin of the flockr
+    user0_details = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    u_id0, token0 = user0_details['u_id'], user0_details['token']
 
-    channel_info = channels_create(admin['token'], "ch_name0", True)
+    # user1 with u_id1 and token1 is not admin
+    user1_details = auth.auth_register("user1@email.com", "user1_pass", "user1_first", "user1_last")
+    token1 = user1_details['token']
+
+    # token0 creates channel, therefore user0 is member and owner of that channel
+    channel_info = channels_create(token0, "ch_name0", True)
     channel_id = channel_info['channel_id']
 
-    channel_join(user0['token'], channel_id)
+    # token1 joins channel, therefore is a member but not an owner
+    channel_join(token1, channel_id)
 
     # assert that channel_addowner raises AccessError
     with pytest.raises(AccessError):
-        channel_removeowner(user0['token'], channel_id, admin['u_id'])
+        channel_removeowner(token1, channel_id, u_id0)
 
 # test that channel_removeowner raises AccessError
 # if the authorised user is not an owner of the channel or the flockr
@@ -136,14 +203,15 @@ def test_channel_removeowner_authnotowner():
 def test_channel_removeowner_accesserror():
     clear()
 
-    admin = auth.auth_register("admin@email.com", "admin_pass", "admin_first", "admin_last")
+    user_details = auth.auth_register("user0@email.com", "user0_pass", "user0_first", "user0_last")
+    u_id, token = user_details['u_id'], user_details['token']
 
-    channel_info = channels_create(admin['token'], "ch_name2", True)
+    channel_info = channels_create(token, "ch_name0", True)
     channel_id = channel_info['channel_id']
 
     # assume " " is always an invalid token
-    invalid_token = " "
+    token = " "
 
     # assert that channel_removeowner raises AccessError
     with pytest.raises(AccessError):
-        channel_removeowner(invalid_token, channel_id, admin['u_id'])
+        channel_removeowner(token, channel_id, u_id)
