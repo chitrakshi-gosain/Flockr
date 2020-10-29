4337009fb8744a66cc592746936b0ce9689a35f1
Joseph Knox <josephdixonknox@gmail.com>
2020-10-29 19:51:54 +1100

    preliminary implementation for message_react and unreact

--- src/message.py
+++ src/message.py
@@ -10,6 +10,7 @@ from helper import get_user_info, get_channel_info, is_user_in_channel, \
     get_message_info, is_user_authorised
 import data
 from error import InputError, AccessError
+from other import search
 
 '''
 ****************************BASIC TEMPLATE******************************
@@ -240,7 +241,7 @@ def message_react(token, message_id, react_id):
         -> message_id : id of the message to be reacted
         -> react_id : id of the react, presently only possibility is 1
                       for thumbs up
-    
+
     EXCEPTIONS:
     Error type: AccessError
         -> token passed in is not a valid token
@@ -253,12 +254,44 @@ def message_react(token, message_id, react_id):
            with ID react_id from the authorised user
     '''
 
-    # Checking for AccessError:
+    # check if token is valid
+    user_info = get_user_info("token", token)
+    if not user_info:
+        raise AccessError('Token is invalid')
 
-    # Checking for InputError(s):
+    # check if message_id is valid
+    # search with empty query_str returns list of all messages from channels including user
+    message_list = search(token, '')
+    if message_id not in [message['message_id'] for message in message_list]:
+        raise InputError('Message ID is invalid')
+
+    # check if react_id is valid
+    react_ids = [react['react_id'] for react in message['reacts'] for message in message_list]
+    if react_id not in react_ids:
+        raise InputError('React ID is invalid')
+
+    # check if user has already reacted
+    for message in message_list:
+        if message['message_id'] == message_id:
+            for react in message['reacts']:
+                if react['react_id'] == react_id:
+                    if react['is_this_user_reacted']:
+                        raise InputError('User has already reacted')
 
     # Since there are no AccessError or InputError(s), hence proceeding
     # forward:
+
+    u_id = user_info['u_id']
+    # find the react with react_id in the message with message_id
+    # append u_id to the react's list of u_ids
+    for channel in data.data['channels']:
+        if is_user_in_channel(u_id, channel['channel_id']):
+            for message in channel['messages']:
+                if message['message_id'] == message_id:
+                    for react in message['reacts']:
+                        if react['react_id'] == react_id:
+                            react['u_ids'].append(u_id)
+                            break
 
     return {
     }
@@ -286,12 +319,44 @@ def message_unreact(token, message_id, react_id):
            with ID react_id
     '''
 
-    # Checking for AccessError:
+    # check if token is valid
+    user_info = get_user_info("token", token)
+    if not user_info:
+        raise AccessError('Token is invalid')
 
-    # Checking for InputError(s):
+    # check if message_id is valid
+    # search with empty query_str returns list of all messages from channels including user
+    message_list = search(token, '')
+    if message_id not in [message['message_id'] for message in message_list]:
+        raise InputError('Message ID is invalid')
+
+    # check if react_id is valid
+    react_ids = [react['react_id'] for react in message['reacts'] for message in message_list]
+    if react_id not in react_ids:
+        raise InputError('React ID is invalid')
+
+    # check if user has not reacted
+    for message in message_list:
+        if message['message_id'] == message_id:
+            for react in message['reacts']:
+                if react['react_id'] == react_id:
+                    if not react['is_this_user_reacted']:
+                        raise InputError('User has not reacted')
 
     # Since there are no AccessError or InputError(s), hence proceeding
     # forward:
+
+    u_id = user_info['u_id']
+    # find the react with react_id in the message with message_id
+    # remove u_id from the react's list of u_ids
+    for channel in data.data['channels']:
+        if is_user_in_channel(u_id, channel['channel_id']):
+            for message in channel['messages']:
+                if message['message_id'] == message_id:
+                    for react in message['reacts']:
+                        if react['react_id'] == react_id:
+                            react['u_ids'].remove(u_id)
+                            break
 
     return {
     }
