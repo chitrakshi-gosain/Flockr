4ea6c8fefe7087ffb3e4d8e3c6724b6dc6d35a71
Ahmet Karatas <z5311022@ad.unsw.edu.au>
2020-11-03 21:37:52 +1100

    adding message_unpin tests

--- src/logic_tests/message_unpin_test.py
+++ src/logic_tests/message_unpin_test.py
@@ -7,7 +7,7 @@ Iteration 2
 
 import pytest
 from channel import channel_join
-from message import message_pin
+from message import message_pin, message_unpin
 from other import search
 from error import InputError, AccessError
 
@@ -20,6 +20,7 @@ FUNCTIONS_USED_FOR_THIS_TEST(PARAMETERS) return {RETURN_VALUES}:
 -> auth_register(email, password, firstname, lastname)
 -> channels_create(token) return {channel_id}
 -> message_pin(token, message_id) return {}
+-> message_unpin(token, message_id) return {}
 -> message_send(token, channel_id, message) return {message_id}
 -> channel_join(token, channel_id) return {}
 -> search(token, query_str) return {messages}
@@ -65,24 +66,22 @@ def test_invalid_message_id(initialise_user_data, initialise_channel_data):
     with pytest.raises(InputError):
         message_pin(owner_credentials['token'], incorrect_message_id)
 
-def test_message_already_pinned(initialise_user_data, initialise_channel_data):
+def test_message_already_unpinned(initialise_user_data, initialise_channel_data):
     owner_credentials = initialise_user_data['owner']
     channel1_id = initialise_channel_data['owner_priv']
     message1_id = message_send(owner_credentials['token'], channel1_id['channel_id'], "Message to be pinned.")
-    message_pin(token, message1_id['message_id'])
     with pytest.raises(InputError):
-        message_pin(token, message1_id['message_id'])
+        message_unpin(token, message1_id['message_id'])
 
 
 def test_user_not_authorised(initialise_user_data, initialise_channel_data):
     owner_credentials = initialise_user_data['owner']
     user1_credentials = initialise_user_data['user1']  
-
     channel1_id = initialise_channel_data['owner_priv']
     message1_id = message_send(user1_credentials['token'], channel1_id['channel_id'], "First message in this channel.")
 
     with pytest.raises(AccessError):
-        message_pin(owner_credentials['token'], message1_id['message_id'])
+        message_unpin(owner_credentials['token'], message1_id['message_id'])
 
 def test_user_not_owner():
     owner_credentials = initialise_user_data['owner']
@@ -90,23 +89,25 @@ def test_user_not_owner():
     message1_id = message_send(owner_credentials['token'], channel1_id['channel_id'], "First message in this channel.")
     user1_credentials = initialise_user_data['user1']
     with pytest.raises(AccessError):
-        message_pin(user1_credentials['token'], message1_id['message_id'])
+        message_unpin(user1_credentials['token'], message1_id['message_id'])
 
 def test_sample_case():
     admin_credentials = initialise_user_data['admin']
     channel1_id = initialise_channel_data['admin_priv']
 
-    messages = get_messages(admin_credentials['token'])
-    for message in messages:
-        if message['message_id'] == message1_id['message_id']:
-            assert message['is_pinned'] == False
-            
     message1_id = message_send(admin_credentials['token'], channel1_id['channel_id'], "The date for the AGM is 14th of November, 2020")
-    
+    message_pin(owner_credentials['token'], message1_id['message_id'])
+
     messages = get_messages(admin_credentials['token'])
     for message in messages:
         if message['message_id'] == message1_id['message_id']:
             assert message['is_pinned'] == True
+            
+    message_unpin(owner_credentials['token'], message1_id['message_id'])
+    messages = get_messages(admin_credentials['token'])
+    for message in messages:
+        if message['message_id'] == message1_id['message_id']:
+            assert message['is_pinned'] == False
 
 
 
