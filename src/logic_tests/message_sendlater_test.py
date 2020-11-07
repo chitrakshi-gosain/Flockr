c41fa684fe087de428d2eeb5a0d7082393f6ae92
Cyrus Wilkie <james.wilkiec@gmail.com>
2020-11-07 11:36:34 +1100

    Added basic valid test for message_sendlater

--- src/logic_tests/message_sendlater_test.py
+++ src/logic_tests/message_sendlater_test.py
@@ -6,8 +6,9 @@ Iteration 3
 '''
 
 import pytest
+from datetime import timezone, datetime
 from auth import auth_logout
-from user import user_profile, user_profile_uploadphoto
+from message import message_sendlater
 from error import AccessError, InputError
 
 '''
@@ -17,7 +18,7 @@ from error import AccessError, InputError
 '''
 FUNCTIONS_USED_FOR_THIS_TEST(PARAMETERS) return {RETURN_VALUES}:
 -> message_sendlater(token, channel_id, message, time_sent) return
-   {messsage_id}
+   {message_id}
 -> auth_register(email, password, name_first, name_last) return
    {u_id, token}
 -> auth_logout(token) return {is_success}
@@ -27,6 +28,7 @@ FUNCTIONS_USED_FOR_THIS_TEST(PARAMETERS) return {RETURN_VALUES}:
 FIXTURES_USED_FOR_THIS_TEST (available in src/logic_tests/conftest.py)
 -> reset
 -> initialise_user_data
+-> initialise_channel_data
 '''
 
 '''
@@ -40,3 +42,19 @@ Error type: AccessError
     -> The authorised user has not joined the channel they are trying
        to post to
 '''
+
+def test_message_sendlater_valid(initialise_user_data, initialise_channel_data):
+    '''
+    Testing a basic valid case
+    '''
+    users = initialise_user_data
+    channels = initialise_channel_data
+
+    curr_time = datetime.now()
+    time_sent = curr_time + datetime(0, 0, 0, 0, 1, 0, 0)
+
+    message_id = message_sendlater(users['user0']['token'], channels['user0_publ']['channel_id'], 
+        'Hello World!', time_sent)
+
+    assert isInstance(message_id['message_id'], int)
+
